3a5e4d8c1bfe9f97006cb4bacf0ff7572b5acef6
matblissim <144238256+matblissim@users.noreply.github.com>
2024-12-30 11:40:12 +0100

    schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -3,20 +3,20 @@
 
     {# Dictionnaire des schémas et leurs tables correspondantes #}
     {%- set schema_mapping = {
-        'sales_tmp': ['box_sales','box_refunds'],
+        'sales_tmp': ['box_sales'],
         'product_tmp': ['catalog', 'algolia_product_categories', 'categories', 'kit_costs', 'nice_names', 'all_kits'],
         'ops_tmp': ['logistics_costs'],
         'snippets_tmp': ['current_box']
     } -%}
 
-    {# Trouver le schéma correspondant #}
+    {# Générer le schéma en vérifiant les noms des tables #}
     {%- for schema, tables in schema_mapping.items() -%}
         {%- if node.name in tables -%}
-            {{ schema }}
-            {%- break -%}
+            {{ schema.strip() }}
+            {%- return -%}  {# Terminer dès qu'un schéma correspondant est trouvé #}
         {%- endif -%}
     {%- endfor -%}
 
-    {# Si aucun schéma spécifique n'est trouvé, retourner le schéma par défaut #}
-    {{ default_schema }}
+    {# Si aucun schéma spécifique trouvé, retourner le schéma par défaut #}
+    {{ default_schema.strip() }}
 {%- endmacro %}
