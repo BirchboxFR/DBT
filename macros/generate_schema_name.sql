2fc4a5ade7cc2294a3bfaab4490098ec583e548e
matblissim <144238256+matblissim@users.noreply.github.com>
2024-12-30 11:47:21 +0100

    revert schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,25 +1,21 @@
 {% macro generate_schema_name(custom_schema_name=none, node=none) -%}
     {%- set default_schema = target.schema -%}
 
-    {# Dictionnaire des schémas et leurs tables correspondantes #}
-    {%- set schema_mapping = {
-        'sales_tmp': ['box_sales'],
-        'product_tmp': ['catalog', 'algolia_product_categories', 'categories', 'kit_costs', 'nice_names', 'all_kits'],
-        'ops_tmp': ['logistics_costs'],
-        'snippets_tmp': ['current_box']
-    } -%}
+    {# Listes des tables spécifiques pour chaque schéma #}
+    {%- set sales_tmp_tables = ['box_sales'] -%}
+    {%- set ops_tmp_tables = ['logistics_costs'] -%}
+    {%- set snippets_tmp_tables = ['current_box'] -%}
+    {%- set product_tmp_tables = ['catalog', 'algolia_product_categories', 'categories','kit_costs','nice_names'] -%}
 
-    {# Initialiser une variable pour stocker le schéma sélectionné #}
-    {%- set selected_schema = default_schema -%}
-
-    {# Vérifier si le nom du modèle correspond à une liste de tables #}
-    {%- for schema, tables in schema_mapping.items() -%}
-        {%- if node.name in tables -%}
-            {%- set selected_schema = schema -%}
-            {%- break -%} {# Sortir de la boucle après avoir trouvé une correspondance #}
-        {%- endif -%}
-    {%- endfor -%}
-
-    {# Retourner le schéma sélectionné #}
-    {{ selected_schema }}
+    {%- if node.name in sales_tmp_tables -%}
+        sales_tmp
+    {%- elif node.name in product_tmp_tables -%}
+        product_tmp
+    {%- elif node.name in ops_tmp_tables -%}
+        ops_tmp
+    {%- elif node.name in snippets_tmp_tables -%}
+        snippets_tmp
+    {%- else -%}
+        {{ default_schema }}
+    {%- endif -%}
 {%- endmacro %}
