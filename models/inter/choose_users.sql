e368307a81cc7e9f500999861b4340cf15f9935c
matblissim <mathieu.helie@blissim.fr>
2025-01-06 17:28:25 +0100

    ajout model choose_users

---
+++ models/inter/choose_users.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "box_id",
+      "data_type": "int64",
+      "range": {
+        "start": 0,
+        "end": 300,
+        "interval": 1
+      }
+    },
+    cluster_by=['dw_country_code']
+) }}
+
+
+
+SELECT 'FR' AS dw_country_code, t.* FROM `bdd_prod_fr.wp_jb_choose_users` t
+UNION ALL
+SELECT 'DE' AS dw_country_code, t.* FROM `bdd_prod_de.wp_jb_choose_users` t
+UNION ALL
+SELECT 'ES' AS dw_country_code, t.* FROM `bdd_prod_es.wp_jb_choose_users` t
+UNION ALL
+SELECT 'IT' AS dw_country_code, t.* FROM `bdd_prod_it.wp_jb_choose_users` t;
