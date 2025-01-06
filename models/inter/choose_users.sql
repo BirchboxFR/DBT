bb28fe6f5f9823a5e16b8f6012adb9979aaa5614
matblissim <mathieu.helie@blissim.fr>
2025-01-06 17:45:42 +0100

    choose_users

--- models/inter/choose_users.sql
+++ models/inter/choose_users.sql
@@ -20,4 +20,4 @@ SELECT 'DE' AS dw_country_code, t.* FROM `bdd_prod_de.wp_jb_choose_users` t
 UNION ALL
 SELECT 'ES' AS dw_country_code, t.* FROM `bdd_prod_es.wp_jb_choose_users` t
 UNION ALL
-SELECT 'IT' AS dw_country_code, t.* FROM `bdd_prod_it.wp_jb_choose_users` t;
+SELECT 'IT' AS dw_country_code, t.* FROM `bdd_prod_it.wp_jb_choose_users` t
