cb9d05111106c04f3e928aed43eef268d49ecac7
matblissim <mathieu.helie@blissim.fr>
2025-01-08 14:36:12 +0100

    config yml

--- models/customer/segmentation/segments.sql
+++ models/customer/segmentation/segments.sql
@@ -1,6 +1,8 @@
+
+/* possibilité sinon c'est dans le yml
 {{ config(
     database='normalised-417010',
     schema='user'
-) }}
-
+- }}
+*/
 select count(*) as nb from teamdata-291012.user.customers 
