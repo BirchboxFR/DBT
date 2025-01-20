6308fa566dd591f51da66646c415e579127c251d
matblissim <mathieu.helie@blissim.fr>
2025-01-20 11:41:58 +0100

    ;

--- models/dwh/inter/views/partial_box_paid.sql
+++ models/dwh/inter/views/partial_box_paid.sql
@@ -5,4 +5,4 @@ SELECT 'DE' AS dw_country_code,t.* FROM `bdd_prod_de.wp_jb_partial_box_paid` t
 UNION ALL
 SELECT 'ES' AS dw_country_code,t.* FROM `bdd_prod_es.wp_jb_partial_box_paid` t
 UNION ALL
-SELECT 'IT' AS dw_country_code,t.* FROM `bdd_prod_it.wp_jb_partial_box_paid` t;
+SELECT 'IT' AS dw_country_code,t.* FROM `bdd_prod_it.wp_jb_partial_box_paid` t
