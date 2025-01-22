c5e64b2e56b0e9b5242aed1ff3aab0e0ea7ab134
matblissim <mathieu.helie@blissim.fr>
2025-01-22 11:15:12 +0100

    tables inter

--- models/dwh/inter/materialized_views/refresh_1h/payments.sql
+++ models/dwh/inter/materialized_views/refresh_1h/payments.sql
@@ -1,7 +1,7 @@
-SELECT 'DE' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date),created_at,updated_at  FROM `bdd_prod_de.wp_jb_payments`
+SELECT 'DE' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date)date,created_at,updated_at  FROM `bdd_prod_de.wp_jb_payments`
 UNION ALL
-SELECT 'ES' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date),created_at,updated_at  FROM `bdd_prod_es.wp_jb_payments`
+SELECT 'ES' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date)date,created_at,updated_at  FROM `bdd_prod_es.wp_jb_payments`
 UNION ALL
-SELECT 'IT' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date),created_at,updated_at  FROM `bdd_prod_it.wp_jb_payments`
+SELECT 'IT' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date)date,created_at,updated_at  FROM `bdd_prod_it.wp_jb_payments`
 union all
-SELECT 'FR' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date),created_at,updated_at  FROM `bdd_prod_fr.wp_jb_payments`
+SELECT 'FR' AS dw_country_code, id,user_id,order_id,sub_id,payment_gateway_id,transaction_id,amount,payment_profile_id,auto,status_id, payment_method_id,data,date(date)date,created_at,updated_at  FROM `bdd_prod_fr.wp_jb_payments`
--- models/dwh/inter/materialized_views/refresh_1h/survey_results.sql
+++ models/dwh/inter/materialized_views/refresh_1h/survey_results.sql
@@ -1,7 +1,10 @@
-SELECT 'FR' AS dw_country_code, t.* FROM `bdd_prod_fr.wp_jb_survey_results` t
+SELECT 'IT' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_it.wp_jb_survey_results` 
 UNION ALL
-SELECT 'DE' AS dw_country_code, t.* FROM `bdd_prod_de.wp_jb_survey_results` t
+SELECT 'DE' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_de.wp_jb_survey_results` 
+where _sdc_deleted_at is null
 UNION ALL
-SELECT 'ES' AS dw_country_code, t.* FROM `bdd_prod_es.wp_jb_survey_results` t
+SELECT 'ES' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_es.wp_jb_survey_results` 
+where _sdc_deleted_at is null
 UNION ALL
-SELECT 'IT' AS dw_country_code, t.* FROM `bdd_prod_it.wp_jb_survey_results` t
+SELECT 'FR' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_fr.wp_jb_survey_results` 
+where _sdc_deleted_at is null
