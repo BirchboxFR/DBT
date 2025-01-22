bc1ee260b491c63f17b7c4285dc44c0b73de5673
matblissim <mathieu.helie@blissim.fr>
2025-01-22 14:26:05 +0100

    test

--- models/schedule/hourly/30mn/teamdata/test_da_eu.sql
+++ models/schedule/hourly/30mn/teamdata/test_da_eu.sql
@@ -1,28 +1,12 @@
-{% set check_deleted_fr = adapter.get_columns_in_relation(source('bdd_prod_fr', 'da_eu_countries')) %}
-{% set check_deleted_de = adapter.get_columns_in_relation(source('bdd_prod_de', 'da_eu_countries')) %}
-{% set check_deleted_es = adapter.get_columns_in_relation(source('bdd_prod_es', 'da_eu_countries')) %}
-{% set check_deleted_it = adapter.get_columns_in_relation(source('bdd_prod_it', 'da_eu_countries')) %}
+{% set tables = ['bdd_prod_fr', 'bdd_prod_de', 'bdd_prod_es', 'bdd_prod_it'] %}
+{% set countries = ['FR', 'DE', 'ES', 'IT'] %}
 
-SELECT 
- 'FR' AS dw_country_code,
- t.*,
- {% if '__deleted' in check_deleted_fr | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
-FROM `bdd_prod_fr.da_eu_countries` t
-UNION ALL 
-SELECT 
- 'DE' AS dw_country_code,
- t.*,
- {% if '__deleted' in check_deleted_de | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
-FROM `bdd_prod_de.da_eu_countries` t
-UNION ALL
-SELECT 
- 'ES' AS dw_country_code,
- t.*,
- {% if '__deleted' in check_deleted_es | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
-FROM `bdd_prod_es.da_eu_countries` t
-UNION ALL
-SELECT 
- 'IT' AS dw_country_code,
- t.*,
- {% if '__deleted' in check_deleted_it | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
-FROM `bdd_prod_it.da_eu_countries` t
+{% for table, country in zip(tables, countries) %}
+ {% set check_deleted = adapter.get_columns_in_relation(ref(table)) %}
+ SELECT 
+   '{{country}}' AS dw_country_code,
+   t.*,
+   {% if '__deleted' in check_deleted | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
+ FROM `{{table}}.da_eu_countries` t
+ {% if not loop.last %}UNION ALL{% endif %}
+{% endfor %}
