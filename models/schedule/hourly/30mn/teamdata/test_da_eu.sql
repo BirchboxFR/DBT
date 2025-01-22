ec7f2ed48d080cc935881a51dae4f78b70456fbc
matblissim <mathieu.helie@blissim.fr>
2025-01-22 15:17:57 +0100

    colu

--- models/schedule/hourly/30mn/teamdata/test_da_eu.sql
+++ models/schedule/hourly/30mn/teamdata/test_da_eu.sql
@@ -1,6 +1,6 @@
-{%- set columns = adapter.get_columns_in_relation(this) -%}
+{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='da_eu_countries')) -%}
 
 SELECT 'FR' AS dw_country_code,
 t.* 
 FROM `bdd_prod_fr.da_eu_countries` t
-WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
+WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
