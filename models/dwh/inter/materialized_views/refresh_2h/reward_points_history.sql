ad1f3bc65bc51cb0af58e4e5614c5781f163e5aa
SoniaBocca <sonia.bocca@blissim.fr>
2025-01-22 16:39:12 +0100

    add deleted on materialized views

--- models/dwh/inter/materialized_views/refresh_2h/reward_points_history.sql
+++ models/dwh/inter/materialized_views/refresh_2h/reward_points_history.sql
@@ -1,7 +1,62 @@
-SELECT 'FR' AS dw_country_code, t.* FROM `bdd_prod_fr.wp_jb_reward_points_history` t
+{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='da_eu_countries')) -%}
+{%- set de_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_de', identifier='da_eu_countries')) -%}
+{%- set es_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_es', identifier='da_eu_countries')) -%}
+{%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='da_eu_countries')) -%}
+
+SELECT 'FR' AS dw_country_code,
+t.* EXCEPT(
+ {% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in fr_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+) 
+FROM `bdd_prod_fr.da_eu_countries` t
+WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'DE' AS dw_country_code, t.* FROM `bdd_prod_de.wp_jb_reward_points_history` t
+
+SELECT 'DE' AS dw_country_code,
+t.* EXCEPT(
+ {% if '__deleted' in de_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in de_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+) 
+FROM `bdd_prod_de.da_eu_countries` t
+WHERE {% if '__deleted' in de_columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'ES' AS dw_country_code, t.* FROM `bdd_prod_es.wp_jb_reward_points_history` t
+
+SELECT 'ES' AS dw_country_code,
+t.* EXCEPT(
+ {% if '__deleted' in es_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in es_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+) 
+FROM `bdd_prod_es.da_eu_countries` t
+WHERE {% if '__deleted' in es_columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'IT' AS dw_country_code, t.* FROM `bdd_prod_it.wp_jb_reward_points_history` t
+
+SELECT 'IT' AS dw_country_code,
+t.* EXCEPT(
+ {% if '__deleted' in it_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in it_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+) 
+FROM `bdd_prod_it.da_eu_countries` t
+WHERE {% if '__deleted' in it_columns | map(attribute='name') %}t.__deleted is null {% else %}true{% endif %}
