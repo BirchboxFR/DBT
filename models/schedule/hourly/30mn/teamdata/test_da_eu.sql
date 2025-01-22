6ff056083c850c7818e2f5c55dcc4b804883884f
matblissim <mathieu.helie@blissim.fr>
2025-01-22 14:29:52 +0100

    da_eu

--- models/schedule/hourly/30mn/teamdata/test_da_eu.sql
+++ models/schedule/hourly/30mn/teamdata/test_da_eu.sql
@@ -1,12 +1,15 @@
-{% set tables = ['bdd_prod_fr', 'bdd_prod_de', 'bdd_prod_es', 'bdd_prod_it'] %}
-{% set countries = ['FR', 'DE', 'ES', 'IT'] %}
-
-{% for table, country in zip(tables, countries) %}
- {% set check_deleted = adapter.get_columns_in_relation(ref(table)) %}
- SELECT 
-   '{{country}}' AS dw_country_code,
-   t.*,
-   {% if '__deleted' in check_deleted | map(attribute='name') %}t.__deleted{% else %}false{% endif %} as is_deleted
- FROM `{{table}}.da_eu_countries` t
- {% if not loop.last %}UNION ALL{% endif %}
-{% endfor %}
+SELECT 'FR' AS dw_country_code, t.* 
+FROM `bdd_prod_fr.da_eu_countries` t
+WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+UNION ALL 
+SELECT 'DE' AS dw_country_code, t.*
+FROM `bdd_prod_de.da_eu_countries` t
+WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+UNION ALL
+SELECT 'ES' AS dw_country_code, t.*
+FROM `bdd_prod_es.da_eu_countries` t
+WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+UNION ALL
+SELECT 'IT' AS dw_country_code, t.*
+FROM `bdd_prod_it.da_eu_countries` t
+WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
