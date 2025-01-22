68ed6007cd319ef9f48ea2c94b0fd53d9ff68c60
matblissim <mathieu.helie@blissim.fr>
2025-01-22 14:33:57 +0100

    ttest

--- models/schedule/hourly/30mn/teamdata/test_da_eu.sql
+++ models/schedule/hourly/30mn/teamdata/test_da_eu.sql
@@ -1,15 +1,15 @@
 SELECT 'FR' AS dw_country_code, t.* 
 FROM `bdd_prod_fr.da_eu_countries` t
-WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+WHERE {% if '__deleted' in columns | map(attribute='name') %}__deleted = false{% endif %}
 UNION ALL 
 SELECT 'DE' AS dw_country_code, t.*
 FROM `bdd_prod_de.da_eu_countries` t
-WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+WHERE {% if '__deleted' in columns | map(attribute='name') %}__deleted = false{% endif %}
 UNION ALL
 SELECT 'ES' AS dw_country_code, t.*
 FROM `bdd_prod_es.da_eu_countries` t
-WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+WHERE {% if '__deleted' in columns | map(attribute='name') %}__deleted = false{% endif %}
 UNION ALL
 SELECT 'IT' AS dw_country_code, t.*
 FROM `bdd_prod_it.da_eu_countries` t
-WHERE {% if '__deleted' in columns | map(attribute='name') %}t.__deleted = false{% else %}true{% endif %}
+WHERE {% if '__deleted' in columns | map(attribute='name') %}__deleted = false{% endif %}
