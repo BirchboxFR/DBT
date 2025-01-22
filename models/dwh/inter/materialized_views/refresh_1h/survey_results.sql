4b6531676c6c0dd7188e4801b694dbc25a3768b4
matblissim <mathieu.helie@blissim.fr>
2025-01-22 11:23:53 +0100

    warehouse

--- models/dwh/inter/materialized_views/refresh_1h/survey_results.sql
+++ models/dwh/inter/materialized_views/refresh_1h/survey_results.sql
@@ -1,10 +1,10 @@
-SELECT 'IT' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_it.wp_jb_survey_results` 
+SELECT 'IT' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at)updated_at, user_id FROM `bdd_prod_it.wp_jb_survey_results` 
 UNION ALL
-SELECT 'DE' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_de.wp_jb_survey_results` 
+SELECT 'DE' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at)updated_at, user_id FROM `bdd_prod_de.wp_jb_survey_results` 
 where _sdc_deleted_at is null
 UNION ALL
-SELECT 'ES' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_es.wp_jb_survey_results` 
+SELECT 'ES' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at)updated_at, user_id FROM `bdd_prod_es.wp_jb_survey_results` 
 where _sdc_deleted_at is null
 UNION ALL
-SELECT 'FR' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at), user_id FROM `bdd_prod_fr.wp_jb_survey_results` 
+SELECT 'FR' AS dw_country_code, order_detail_id, survey_id, sub_id, order_id, status, timestamp, id, datetime(updated_at)updated_at, user_id FROM `bdd_prod_fr.wp_jb_survey_results` 
 where _sdc_deleted_at is null
