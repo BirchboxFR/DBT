b07c7050c7acabad23590247b2cb5275df7e2fdb
matblissim <mathieu.helie@blissim.fr>
2025-01-23 17:11:54 +0100

    invoices

--- models/dwh/inter/views/invoices.sql
+++ models/dwh/inter/views/invoices.sql
@@ -1,23 +1,62 @@
 
 
 --- test de la table invoices
-SELECT 'FR' AS dw_country_code,t.*except(invoice_date,order_date,payment_date), 
+SELECT 'FR' AS dw_country_code,t.*except(invoice_date,order_date,payment_date,
+{% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in fr_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}), 
 safe_cast(invoice_date as date) as invoice_date,
 safe_cast(payment_date as date) as payment_date,
 safe_cast(order_date as date) as order_date
  FROM `bdd_prod_fr.wp_jb_invoices` t
+ WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'DE' AS dw_country_code,t.*except(invoice_date,order_date,payment_date), 
+SELECT 'DE' AS dw_country_code,t.*except(invoice_date,order_date,payment_date,
+{% if '__deleted' in de_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in de_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ ), 
 safe_cast(invoice_date as date) as invoice_date,
 safe_cast(payment_date as date) as payment_date,
-safe_cast(order_date as date) as order_date FROM `bdd_prod_de.wp_jb_invoices` t
+safe_cast(order_date as date) as order_date 
+FROM `bdd_prod_de.wp_jb_invoices` t
+WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'ES' AS dw_country_code,t.*except(invoice_date,order_date,payment_date), 
+SELECT 'ES' AS dw_country_code,t.*except(invoice_date,order_date,payment_date,
+{% if '__deleted' in es_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in es_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}), 
 safe_cast(invoice_date as date) as invoice_date,
 safe_cast(payment_date as date) as payment_date,
-safe_cast(order_date as date) as order_date FROM `bdd_prod_es.wp_jb_invoices` t
+safe_cast(order_date as date) as order_date 
+FROM `bdd_prod_es.wp_jb_invoices` t
+WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+
 UNION ALL
-SELECT 'IT' AS dw_country_code,t.*except(invoice_date,order_date,payment_date), 
+SELECT 'IT' AS dw_country_code,t.*except(invoice_date,order_date,payment_date,
+{% if '__deleted' in it_columns | map(attribute='name') %}__deleted,{% endif %}
+ {% if '__ts_ms' in it_columns | map(attribute='name') %}__ts_ms,{% endif %}
+ {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
+ {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
+ {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
+ {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}), 
 safe_cast(invoice_date as date) as invoice_date,
 safe_cast(payment_date as date) as payment_date,
-safe_cast(order_date as date) as order_date FROM `bdd_prod_it.wp_jb_invoices` t
+safe_cast(order_date as date) as order_date 
+FROM `bdd_prod_it.wp_jb_invoices` t
+WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
