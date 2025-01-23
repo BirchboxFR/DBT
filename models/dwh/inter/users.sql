60b098821b743873e26f3f9e56e0d0b478bcae0f
matblissim <144238256+matblissim@users.noreply.github.com>
2025-01-23 17:54:27 +0100

    users

--- models/dwh/inter/users.sql
+++ models/dwh/inter/users.sql
@@ -4,7 +4,7 @@
 {%- set es_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_es', identifier='wp_users')) -%}
 {%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='wp_users')) -%}
 
-SELECT 'FR' AS dw_country_code, u.*except(user_birthday,
+SELECT 'FR' AS dw_country_code, t.*except(user_birthday,
  {% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
  {% if '__ts_ms' in fr_columns | map(attribute='name') %}__ts_ms,{% endif %}
  {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
@@ -13,11 +13,11 @@ SELECT 'FR' AS dw_country_code, u.*except(user_birthday,
  {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
  {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ),
-safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_fr.wp_users` u
+safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_fr.wp_users` t
 WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 UNION ALL 
-SELECT 'DE' AS dw_country_code, u.*except(user_birthday,
+SELECT 'DE' AS dw_country_code, t.*except(user_birthday,
  {% if '__deleted' in de_columns | map(attribute='name') %}__deleted,{% endif %}
  {% if '__ts_ms' in de_columns | map(attribute='name') %}__ts_ms,{% endif %}
  {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
@@ -25,11 +25,11 @@ SELECT 'DE' AS dw_country_code, u.*except(user_birthday,
  {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
  {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
  {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}),
-safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_de.wp_users` u
+safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_de.wp_users` t
 WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 UNION ALL 
-SELECT 'ES' AS dw_country_code, u.*except(user_birthday,
+SELECT 'ES' AS dw_country_code, t.*except(user_birthday,
  {% if '__deleted' in es_columns | map(attribute='name') %}__deleted,{% endif %}
  {% if '__ts_ms' in es_columns | map(attribute='name') %}__ts_ms,{% endif %}
  {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
@@ -37,11 +37,11 @@ SELECT 'ES' AS dw_country_code, u.*except(user_birthday,
  {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
  {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
  {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}),
-safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_es.wp_users` u
+safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_es.wp_users` t
 WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false)  {% else %}true{% endif %}
 
 UNION ALL 
-SELECT 'IT' AS dw_country_code, u.*except(user_birthday,
+SELECT 'IT' AS dw_country_code, t.*except(user_birthday,
  {% if '__deleted' in it_columns | map(attribute='name') %}__deleted,{% endif %}
  {% if '__ts_ms' in it_columns | map(attribute='name') %}__ts_ms,{% endif %}
  {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
@@ -49,7 +49,7 @@ SELECT 'IT' AS dw_country_code, u.*except(user_birthday,
  {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
  {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
  {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}),
-safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_it.wp_users` u
+safe_cast(user_birthday as date) as user_birthday FROM `bdd_prod_it.wp_users` t
 WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 
