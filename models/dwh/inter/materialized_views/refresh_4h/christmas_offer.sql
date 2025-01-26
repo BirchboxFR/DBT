e0effcff7978358283d8213521097f60fb0992dc
matblissim <144238256+matblissim@users.noreply.github.com>
2025-01-26 18:17:02 +0100

    fichier

--- models/dwh/inter/materialized_views/refresh_4h/christmas_offer.sql
+++ models/dwh/inter/materialized_views/refresh_4h/christmas_offer.sql
@@ -1,3 +1,11 @@
+{{
+  config(
+    materialized='materialized_view',
+    enable_refresh=true,
+    refresh_interval_minutes=120,
+    max_staleness='8:0:0'
+  )
+}}
 
 {%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='da_eu_countries')) -%}
 {%- set de_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_de', identifier='da_eu_countries')) -%}
