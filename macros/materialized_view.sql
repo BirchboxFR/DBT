92dd6263207172f10c38e45d9911c398b2a39355
matblissim <144238256+matblissim@users.noreply.github.com>
2025-01-26 18:31:52 +0100

    string

--- macros/materialized_view.sql
+++ macros/materialized_view.sql
@@ -1,8 +1,11 @@
 {% macro set_materialized_view_settings() %}
-ALTER MATERIALIZED VIEW `teamdata-291012`.`inter`.`christmas_offer`
-SET OPTIONS(
-   enable_refresh=True,
-   refresh_interval_minutes=120,
-   max_staleness=INTERVAL '8:0:0' HOUR TO SECOND
-)
+    {% set query %}
+        alter materialized view {{ this }}
+        set OPTIONS(
+            enable_refresh=True,
+            refresh_interval_minutes=120,
+            max_staleness=INTERVAL '8:0:0' HOUR TO SECOND
+        )
+    {% endset %}
+    {% do run_query(query) %}
 {% endmacro %}
