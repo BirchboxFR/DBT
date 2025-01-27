9ce19665fdcc5cf9a05cad18630ccabeffe9f63e
matblissim <mathieu.helie@blissim.fr>
2025-01-27 14:06:25 +0100

    comment posts

--- models/dwh/inter/views/open_comment_posts.sql
+++ models/dwh/inter/views/open_comment_posts.sql
@@ -9,23 +9,23 @@
 SELECT 'FR' AS dw_country_code,id, post_id, created_at, created_by, 
 safe_cast(start_date as date) as start_date,
 safe_cast(end_date as date) as end_date,
-  FROM `bdd_prod_fr.wp_jb_open_comment_posts` u
+  FROM `bdd_prod_fr.wp_jb_open_comment_posts` t
   WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 UNION ALL
 SELECT 'DE' AS dw_country_code,id, post_id, created_at, created_by,  
 safe_cast(start_date as date) as start_date,
-safe_cast(end_date as date) as end_date, FROM `bdd_prod_de.wp_jb_open_comment_posts` u
+safe_cast(end_date as date) as end_date, FROM `bdd_prod_de.wp_jb_open_comment_posts` t
   WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 UNION ALL
 SELECT 'ES' AS dw_country_code,id, post_id, created_at, created_by,  
 safe_cast(start_date as date) as start_date,
-safe_cast(end_date as date) as end_date, FROM `bdd_prod_es.wp_jb_open_comment_posts` u
+safe_cast(end_date as date) as end_date, FROM `bdd_prod_es.wp_jb_open_comment_posts` t
   WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 
 UNION ALL
 SELECT 'IT' AS dw_country_code,id, post_id, created_at, created_by, 
 safe_cast(start_date as date) as start_date,
-safe_cast(end_date as date) as end_date, FROM `bdd_prod_it.wp_jb_open_comment_posts` u
+safe_cast(end_date as date) as end_date, FROM `bdd_prod_it.wp_jb_open_comment_posts` t
   WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
