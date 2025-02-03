157c219a69a3b5a9720e1ede8eaf3f4021a6b6de
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-03 09:14:03 +0100

    update

--- models/schedule/daily/5am/normalised/today_middle.sql
+++ models/schedule/daily/5am/normalised/today_middle.sql
@@ -1,20 +1,14 @@
 
-select *,'middle' as status from teamdata-291012.user.customers 
-where user_id not in 
 
-(select user_id from {{ ref('today_stars') }}
-union all
-select user_id from {{ ref('today_whales') }}
-union all
-select user_id from {{ ref('today_new') }}
-union all
-select user_id from {{ ref('today_lost') }}
-union all
-select user_id from {{ ref('today_prospects') }}
-union all
-select user_id from {{ ref('today_inactive') }}
-union all
-select user_id from {{ ref('today_risky') }}
-union all
-select user_id from {{ ref('today_spectators') }}
-)
+select distinct t.user_id,'middle' as status,box_sub_status as status_lvl1 
+from teamdata-291012.user.customers t
+left join `normalised-417010`.`user`.`today_prospects` p on p.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_spectators` sp on sp.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_new` n on n.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_whales` w on w.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_stars` stars on stars.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_stars` ri on ri.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_stars` i on i.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_lost` l on l.user_id=t.user_id 
+where p.user_id is null and sp.user_id is null and n.user_id is null and w.user_id is null and stars.user_id is null and ri.user_id is null
+and i.user_id is null and l.user_id is null
--- models/schedule/daily/5am/normalised/today_risky.sql
+++ models/schedule/daily/5am/normalised/today_risky.sql
@@ -1,39 +1,38 @@
-select distinct user_id,status as status_lvl1,'RISKY' as status from (
+select distinct t.user_id,box_sub_status as status_lvl1,'RISKY' as status from (
 --end of gift
-select distinct user_id,'end of gift' as status
+select distinct user_id,'end of gift' as status,box_sub_status
 from sales.box_sales bs
+inner join user.customers c using(user_id,dw_country_code)
 where bs.gift = 1 AND bs.last_committed_box = 1
-and date='2024-11-01' and dw_country_code='FR'
+and date='2025-02-01' and dw_country_code='FR'
 union all
 
 -- moins de 7 box et sub
-select distinct user_id ,'moins de 7 box'
+select distinct user_id ,'moins de 7 box',box_sub_status
 from `teamdata-291012.user.customers`
 where nb_box_paid <7
 and box_sub_status='SUB'
 
 -- les acquiz mega discount
 union all
-select distinct user_id,'mega discount'
+select distinct user_id,'mega discount',box_sub_status
 from sales.box_sales bs
-where  date='2024-11-01' and dw_country_code='FR'
+inner join user.customers c using(user_id,dw_country_code)
+where  date>='2024-01-01' and dw_country_code='FR'
 and bs.acquis_status_lvl2 IN ('NEW NEW','REACTIVATION') AND ( safe_divide(bs.total_discount,total_product) > 0.3) 
-
+and box_sub_status='SUB'
 
 
 
 -- react m-1 et m-2
 
-)
-where user_id not in 
+)t
+left join `normalised-417010`.`user`.`today_prospects` p on p.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_spectators` sp on sp.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_new` n on n.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_whales` w on w.user_id=t.user_id 
+left join `normalised-417010`.`user`.`today_stars` stars on stars.user_id=t.user_id 
+where p.user_id is null and sp.user_id is null and n.user_id is null and w.user_id is null and stars.user_id is null
 
-(select user_id from {{ ref('today_stars') }}
-union all
-select user_id from {{ ref('today_whales') }}
-union all
-select user_id from {{ ref('today_new') }}
-union all
-select user_id from {{ ref('today_spectators') }}
-union all
-select user_id from {{ ref('today_prospects') }}
-)
+group by all
+
