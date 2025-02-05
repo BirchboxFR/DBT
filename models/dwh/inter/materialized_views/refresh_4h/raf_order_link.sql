ec04941abf3d4c28bed3a70013e4851c4e965606
matblissim <mathieu.helie@blissim.fr>
2025-02-05 14:26:22 +0100

    rol 2eme version

--- models/dwh/inter/materialized_views/refresh_4h/raf_order_link.sql
+++ models/dwh/inter/materialized_views/refresh_4h/raf_order_link.sql
@@ -4,60 +4,28 @@
 {%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='wp_jb_raf_order_link')) -%}
 
 SELECT 'FR' AS dw_country_code,
-raf_offer_id,order_id,created_at,updated_at,max(id) as id EXCEPT(
- {% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
- {% if '__ts_ms' in fr_columns | map(attribute='name') %}__ts_ms,{% endif %}
- {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
- {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
- {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
-) 
+max(raf_offer_id) as raf_offer_id,order_id,max(created_at) as created_at ,max(updated_at) as updated_at,max(id) as id 
 FROM `bdd_prod_fr.wp_jb_raf_order_link` t
 WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 group by all
 UNION ALL
 
 SELECT 'DE' AS dw_country_code,
-raf_offer_id,order_id,created_at,updated_at,max(id) as id EXCEPT(
- {% if '__deleted' in de_columns | map(attribute='name') %}__deleted,{% endif %}
- {% if '__ts_ms' in de_columns | map(attribute='name') %}__ts_ms,{% endif %}
- {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
- {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
- {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
-) 
+max(raf_offer_id) as raf_offer_id,order_id,max(created_at) as created_at ,max(updated_at) as updated_at,max(id) as id 
 FROM `bdd_prod_de.wp_jb_raf_order_link` t
 WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 group by all
 UNION ALL
 
 SELECT 'ES' AS dw_country_code,
-raf_offer_id,order_id,created_at,updated_at,max(id) as id EXCEPT(
- {% if '__deleted' in es_columns | map(attribute='name') %}__deleted,{% endif %}
- {% if '__ts_ms' in es_columns | map(attribute='name') %}__ts_ms,{% endif %}
- {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
- {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
- {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
-) 
+max(raf_offer_id) as raf_offer_id,order_id,max(created_at) as created_at ,max(updated_at) as updated_at,max(id) as id 
 FROM `bdd_prod_es.wp_jb_raf_order_link` t
 WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 group by all
 UNION ALL
 
 SELECT 'IT' AS dw_country_code,
-raf_offer_id,order_id,created_at,updated_at,max(id) as id EXCEPT(
- {% if '__deleted' in it_columns | map(attribute='name') %}__deleted,{% endif %}
- {% if '__ts_ms' in it_columns | map(attribute='name') %}__ts_ms,{% endif %}
- {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
- {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
- {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
-) 
+max(raf_offer_id) as raf_offer_id,order_id,max(created_at) as created_at ,max(updated_at) as updated_at,max(id) as id 
 FROM `bdd_prod_it.wp_jb_raf_order_link` t
 WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
 group by all
