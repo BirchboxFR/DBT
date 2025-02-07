6119bb686c1f8f15a930685365cff3ae4420263b
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-07 18:45:29 +0100

    kit correction

--- models/dwh/inter/materialized_views/refresh_4h/kit.sql
+++ models/dwh/inter/materialized_views/refresh_4h/kit.sql
@@ -1,4 +1,6 @@
+{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_sublissim', identifier='kit')) -%}
+
 SELECT 
 t.*
 FROM `bdd_prod_sublissim.kit_raw` t
-WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
