bbcb16a5c66198cdc74fa390e12dfc59ecbd4fc5
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-07 18:48:21 +0100

    kit v4

--- models/dwh/inter/materialized_views/refresh_4h/kit.sql
+++ models/dwh/inter/materialized_views/refresh_4h/kit.sql
@@ -1,4 +1,4 @@
-{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_sublissim', identifier='kit')) -%}
+{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_sublissim', identifier='kit_raw')) -%}
 
 SELECT 
 t.*
