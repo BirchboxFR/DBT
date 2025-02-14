0d7c44816f970719af01de785f799fa90d9c1735
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-14 09:47:38 +0100

    consent V3

--- models/dwh/inter/views/consent.sql
+++ models/dwh/inter/views/consent.sql
@@ -2,6 +2,6 @@
 
 
 SELECT c.*,name 
-FROM {{ ref('user_consent') }} c
+FROM {{ ref('user_consent') }} t
 inner join {{ ref('consent_topic') }} ct using(consent_topic_id)
 WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
