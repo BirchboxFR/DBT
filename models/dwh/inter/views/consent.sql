00c3e1a2dd6d747d2146ce2de5eb11dd8601f4c6
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-14 09:46:10 +0100

    cosnent v2

--- models/dwh/inter/views/consent.sql
+++ models/dwh/inter/views/consent.sql
@@ -1,3 +1,6 @@
+{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='wp_jb_user_consent')) -%}
+
+
 SELECT c.*,name 
 FROM {{ ref('user_consent') }} c
 inner join {{ ref('consent_topic') }} ct using(consent_topic_id)
