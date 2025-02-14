d1351d7f6c03c271f32eaef7b41177c62d3b0541
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-14 10:02:27 +0100

    consent v4

--- models/dwh/inter/views/consent.sql
+++ models/dwh/inter/views/consent.sql
@@ -1,7 +1,5 @@
-{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='wp_jb_user_consent')) -%}
 
 
 SELECT c.*,name 
-FROM {{ ref('user_consent') }} t
-inner join {{ ref('consent_topic') }} ct using(consent_topic_id)
-WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+FROM {{ ref('user_consent') }} c
+inner join {{ ref('consent_topic') }} ct using(consent_topic_id,dw_country_code)
