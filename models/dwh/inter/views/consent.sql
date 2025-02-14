b1e70cee2490aa5121769402ab7cbf060aaa4d50
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-14 09:34:41 +0100

    consent sans les deleted

--- models/dwh/inter/views/consent.sql
+++ models/dwh/inter/views/consent.sql
@@ -1,3 +1,4 @@
 SELECT c.*,name 
 FROM {{ ref('user_consent') }} c
 inner join {{ ref('consent_topic') }} ct using(consent_topic_id)
+WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
