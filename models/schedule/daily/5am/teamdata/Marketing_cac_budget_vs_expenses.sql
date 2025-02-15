cb114afe74e65d08545c53964408fee7a6b69ff0
matblissim <144238256+matblissim@users.noreply.github.com>
2025-02-15 21:23:21 +0100

    marketing budget

--- models/schedule/daily/5am/teamdata/Marketing_cac_budget_vs_expenses.sql
+++ models/schedule/daily/5am/teamdata/Marketing_cac_budget_vs_expenses.sql
@@ -1,4 +1,4 @@
-SELECT distinct month,upper(b.levier),upper(b.levierFC),budget,sum(spent) spent ,safe_divide(sum(spent)  , budget) as ratio_spent
+SELECT distinct month,upper(b.levier) levier,upper(b.levierFC) levierFC,budget,sum(spent) spent ,safe_divide(sum(spent)  , budget) as ratio_spent
 FROM `teamdata-291012.marketing.Marketing_cac_budget` b 
 left join {{ ref('Marketing_cac_expenses') }} e on upper(e.levier)=upper(b.levier) and upper(e.levierFC)=upper(b.levierFC) and e.country=b.country and e.mois=b.Month
 where b.month>='2023-10-01' and b.country='FR'
