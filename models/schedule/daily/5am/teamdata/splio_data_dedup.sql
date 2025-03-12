00514f8b2e68e002bac22672742c908796365d85
matblissim <mathieu.helie@blissim.fr>
2025-03-12 11:44:29 +0100

    splio data

--- models/schedule/daily/5am/teamdata/splio_data_dedup.sql
+++ models/schedule/daily/5am/teamdata/splio_data_dedup.sql
@@ -2,24 +2,29 @@
 {{
   config(
     materialized='incremental',
-    unique_key=['email', 'status','event_date'],
+    unique_key=['email', 'status', 'event_date', 'campaignid'],
     incremental_strategy='merge'
   )
 }}
 
-SELECT * EXCEPT(rn)
+SELECT 
+  ContactID AS email,
+  Status AS status,
+  Event_date AS event_date,
+  CampaignID AS campaignid
 FROM (
   SELECT 
-    ContactID AS email,
-    Status AS status,
-    Event_date AS event_date,
-    ROW_NUMBER() OVER (PARTITION BY CampaignID, ContactID, Status ORDER BY Event_date) rn
+    ContactID,
+    Status,
+    Event_date,
+    CampaignID,
+    ROW_NUMBER() OVER (PARTITION BY CampaignID, ContactID, Status ORDER BY Event_date) AS rn
   FROM crm.splio_events
   WHERE event_date IS NOT NULL 
     AND Event_Date >= '2023-01-01'
     {% if is_incremental() %}
-    -- Si c'est une exécution incrémentale, ne prenez que les données depuis la dernière exécution
-    AND Event_Date >= (SELECT MAX(event_date) FROM {{ this }})
+    -- En mode incrémental, prenez les données du jour précédent pour éviter les conflits
+    AND Event_Date > (SELECT DATE_SUB(MAX(event_date), INTERVAL 1 DAY) FROM {{ this }})
     {% endif %}
 )
 WHERE rn = 1
