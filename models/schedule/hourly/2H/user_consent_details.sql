db440dc981dbe9fc1d30f395bd1f5e4a8d25dc8d
matblissim <mathieu.helie@blissim.fr>
2025-03-12 18:12:34 +0100

    consent detail only 1 email

--- models/schedule/hourly/2H/user_consent_details.sql
+++ models/schedule/hourly/2H/user_consent_details.sql
@@ -1,9 +1,43 @@
-SELECT distinct user_id,user_email,dw_country_code,
-max(case when t.name='email' and consent_status=1 then true else false end )consent_email ,
-max(case when t.name='sms' and consent_status=1 then true else false end) consent_sms,
-max(case when t.name='whatsapp' and consent_status=1 then true else false end) consent_whatsapp ,
-max(case when t.name='partner_sharing' and consent_status=1 then true else false end) consent_partner_sharing ,
-max(case when t.name='beauty_profile' and consent_status=1 then true else false end) consent_beauty_profile ,
-FROM {{ ref('user_consent') }} c
-left join {{ ref('consent_topic') }} t  using (consent_topic_id,dw_country_code)
-group by all
+WITH LatestEmails AS (
+  SELECT
+    user_id,
+    dw_country_code,
+    user_email
+  FROM (
+    SELECT
+      user_id,
+      dw_country_code,
+      user_email,
+      ROW_NUMBER() OVER(PARTITION BY user_id, dw_country_code ORDER BY updated_at DESC) as rn
+    FROM {{ ref('user_consent') }}
+    -- test multi WHERE user_id = 2327271 AND dw_country_code = 'DE'
+  )
+  WHERE rn = 1
+),
+
+ConsentStatus AS (
+  SELECT 
+    user_id,
+    dw_country_code,
+    max(case when t.name='email' and consent_status=1 then true else false end) AS consent_email,
+    max(case when t.name='sms' and consent_status=1 then true else false end) AS consent_sms,
+    max(case when t.name='whatsapp' and consent_status=1 then true else false end) AS consent_whatsapp,
+    max(case when t.name='partner_sharing' and consent_status=1 then true else false end) AS consent_partner_sharing,
+    max(case when t.name='beauty_profile' and consent_status=1 then true else false end) AS consent_beauty_profile
+  FROM {{ ref('user_consent') }} c
+  LEFT JOIN {{ ref('consent_topic') }} t USING (consent_topic_id, dw_country_code)
+  WHERE user_id = 2327271 AND dw_country_code = 'DE'
+  GROUP BY user_id, dw_country_code
+)
+
+SELECT
+  e.user_id,
+  e.user_email,
+  e.dw_country_code,
+  c.consent_email,
+  c.consent_sms,
+  c.consent_whatsapp,
+  c.consent_partner_sharing,
+  c.consent_beauty_profile
+FROM LatestEmails e
+JOIN ConsentStatus c USING (user_id, dw_country_code)
