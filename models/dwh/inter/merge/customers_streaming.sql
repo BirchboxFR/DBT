818dedcb691f0183dd6eb429ff79ac78aad9eb30
matblissim <mathieu.helie@blissim.fr>
2025-03-12 14:54:09 +0100

    customers_streaming

---
+++ models/dwh/inter/merge/customers_streaming.sql
@@ -0,0 +1,29 @@
+WITH all_users AS (
+    SELECT dw_country_code, email,
+        MAX(user_id) AS user_id 
+    FROM (   
+        SELECT 
+            dw_country_code, 
+            email, 
+            NULL AS user_id
+        FROM inter.optin
+        
+        UNION ALL
+        
+        SELECT 
+            dw_country_code, 
+            user_email AS email, 
+            id AS user_id
+        FROM {{ ref('users') }}
+        WHERE user_login <> 'DELETED'
+    ) combined_users
+    GROUP BY dw_country_code, email
+)
+
+SELECT 
+    au.dw_country_code,
+    user_id,
+    case when user_id is null then true else false end as is_prospect,
+    au.email
+FROM all_users au
+left join {{ ref('users') }} u on u.id=au.user_id and u.dw_country_code=au.dw_country_code
