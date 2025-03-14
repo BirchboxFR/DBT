6809e3f2955285988fe130f3cf8733b23fe8a14d
matblissim <mathieu.helie@blissim.fr>
2025-03-14 10:19:18 +0100

    sra V2

--- models/dwh/inter/materialized_views/refresh_4h/survey_result_answers.sql
+++ models/dwh/inter/materialized_views/refresh_4h/survey_result_answers.sql
@@ -22,7 +22,7 @@ t.* EXCEPT(
  {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id{% endif %}
  --{% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_fr.wp_jb_survey_result_answers` t
@@ -37,7 +37,7 @@ t.* EXCEPT(
  {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id{% endif %}
  --{% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_de.wp_jb_survey_result_answers` t
@@ -52,7 +52,7 @@ t.* EXCEPT(
  {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id{% endif %}
  --{% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_es.wp_jb_survey_result_answers` t
@@ -67,7 +67,7 @@ t.* EXCEPT(
  {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
+ {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id{% endif %}
  --{% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_it.wp_jb_survey_result_answers` t
