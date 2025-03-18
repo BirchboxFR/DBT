4790f056dc0dc31319a02e0579fc6893d30f675a
matblissim <mathieu.helie@blissim.fr>
2025-03-18 10:46:18 +0100

    last order CIP

--- models/dwh/inter/merge/customers_info_perso.sql
+++ models/dwh/inter/merge/customers_info_perso.sql
@@ -1,5 +1,5 @@
 
-SELECT distinct user_id ,dw_country_code,last_value(date) over ( partition by user_id order by date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) ,
+SELECT distinct user_id ,dw_country_code,last_value(date) over ( partition by user_id order by date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as last_order_date ,
 last_value(billing_country) over ( partition by user_id order by date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) billing_country,
 last_value(billing_zipcode) over ( partition by user_id order by date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) billing_zipcode,
 last_value(billing_phone) over ( partition by user_id order by date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) billing_phone,
