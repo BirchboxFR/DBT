8ce3c8e35ddd3ea3ba3e4ea6a4a23261bd4e35be
matblissim <mathieu.helie@blissim.fr>
2025-03-19 10:40:25 +0100

    all merge

--- models/dwh/inter/merge/partial_cancelations.sql
+++ models/dwh/inter/merge/partial_cancelations.sql
@@ -1,9 +1,23 @@
+{{ config(
+    partition_by={
+    "field": "_rivery_last_update",
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=['dw_country_code','special_type']
+) }}
+
+--partition 
+{% set lookback_hours = 2 %}
+--lookback 2h
 
 {%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='wp_jb_partial_cancelations')) -%}
 {%- set de_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_de', identifier='wp_jb_partial_cancelations')) -%}
 {%- set es_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_es', identifier='wp_jb_partial_cancelations')) -%}
 {%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='wp_jb_partial_cancelations')) -%}
 
+
+--données FR
 SELECT 'FR' AS dw_country_code,
 t.* EXCEPT(
  {% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
@@ -11,12 +25,24 @@ t.* EXCEPT(
  {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_fr.wp_jb_partial_cancelations` t
-WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+WHERE 
+  -- Filtre sur les lignes non supprimées
+  {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  -- Filtre sur les données récentes uniquement
+  {% if is_incremental() %}
+  (
+    -- Données mises à jour récemment (dans les X dernières heures)
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  -- Premier chargement: toutes les données
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'DE' AS dw_country_code,
@@ -26,12 +52,20 @@ t.* EXCEPT(
  {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_de.wp_jb_partial_cancelations` t
-WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+WHERE 
+  {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'ES' AS dw_country_code,
@@ -41,12 +75,20 @@ t.* EXCEPT(
  {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_es.wp_jb_partial_cancelations` t
-WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+WHERE 
+  {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'IT' AS dw_country_code,
@@ -56,8 +98,17 @@ t.* EXCEPT(
  {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_it.wp_jb_partial_cancelations` t
-WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
+WHERE 
+  {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
+
+  )
+  {% else %}
+  TRUE
+  {% endif %}
--- models/dwh/inter/merge/products_stock_log.sql
+++ models/dwh/inter/merge/products_stock_log.sql
@@ -1,9 +1,22 @@
+{{ config(
+    partition_by={
+    "field": "_rivery_last_update",
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=['dw_country_code','product_id']
+) }}
+
+--partition 
+{% set lookback_hours = 2 %}
+--lookback 2h
 
 {%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='wp_jb_products_stock_log')) -%}
 {%- set de_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_de', identifier='wp_jb_products_stock_log')) -%}
 {%- set es_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_es', identifier='wp_jb_products_stock_log')) -%}
 {%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='wp_jb_products_stock_log')) -%}
 
+--données FR
 SELECT 'FR' AS dw_country_code,
 t.* EXCEPT(
  {% if '__deleted' in fr_columns | map(attribute='name') %}__deleted,{% endif %}
@@ -11,13 +24,24 @@ t.* EXCEPT(
  {% if '__transaction_order' in fr_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in fr_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in fr_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in fr_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in fr_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_fr.wp_jb_products_stock_log` t
-WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
-AND date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH))
+WHERE 
+  -- Filtre sur les lignes non supprimées
+  {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  -- Filtre sur les données récentes uniquement
+  {% if is_incremental() %}
+  (
+    -- Données mises à jour récemment (dans les X dernières heures)
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  -- Premier chargement: toutes les données
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'DE' AS dw_country_code,
@@ -27,13 +51,20 @@ t.* EXCEPT(
  {% if '__transaction_order' in de_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in de_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in de_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in de_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in de_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_de.wp_jb_products_stock_log` t
-WHERE {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
-AND date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH))
+WHERE 
+  {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'ES' AS dw_country_code,
@@ -43,13 +74,20 @@ t.* EXCEPT(
  {% if '__transaction_order' in es_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in es_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in es_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in es_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in es_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_es.wp_jb_products_stock_log` t
-WHERE {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
-AND date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH))
+WHERE 
+  {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
 
+  )
+  {% else %}
+  TRUE
+  {% endif %}
 UNION ALL
 
 SELECT 'IT' AS dw_country_code,
@@ -59,9 +97,17 @@ t.* EXCEPT(
  {% if '__transaction_order' in it_columns | map(attribute='name') %}__transaction_order,{% endif %}
  {% if '__transaction_id' in it_columns | map(attribute='name') %}__transaction_id,{% endif %}
  {% if '_rivery_river_id' in it_columns | map(attribute='name') %}_rivery_river_id,{% endif %}
- {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id,{% endif %}
- {% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
+ {% if '_rivery_run_id' in it_columns | map(attribute='name') %}_rivery_run_id{% endif %}
+ --{% if '_rivery_last_update' in it_columns | map(attribute='name') %}_rivery_last_update{% endif %}
 ) 
 FROM `bdd_prod_it.wp_jb_products_stock_log` t
-WHERE {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
-AND date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 6 MONTH))
+WHERE 
+  {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
+  {% if is_incremental() %}
+  (
+    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
+
+  )
+  {% else %}
+  TRUE
+  {% endif %}
