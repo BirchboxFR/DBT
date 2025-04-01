7e65b4ffd100caa5e9c3e3e9a518a7990f04ef56
matblissim <mathieu.helie@blissim.fr>
2025-04-01 15:19:18 +0200

    full

--- snapshots/history_segments_full.sql
+++ snapshots/history_segments_full.sql
@@ -1,11 +1,11 @@
-{% snapshot history_segments %}
+{% snapshot history_segments_full %}
 
     {{
         config(
           target_database='normalised-417010',  
           target_schema='user',
-          strategy='check',
-          check_cols=['status'],
+          strategy='timestamp',
+          updated_at='snapshot_date',
           invalidate_hard_deletes=True,
           unique_key='user_id',
         )
@@ -14,11 +14,10 @@
     SELECT 
         user_id,
         status,
-        CURRENT_date() AS snapshot_date
+        CURRENT_TIMESTAMP() AS snapshot_date
     FROM (
         SELECT user_id, status
         FROM {{ref('today_segments')}}
-       
     )
 
 {% endsnapshot %}
