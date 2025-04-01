6d0613e5a1aa1dc19111d015b84db8aea6adf23e
matblissim <mathieu.helie@blissim.fr>
2025-04-01 15:31:26 +0200

    risky

--- models/schedule/daily/5am/normalised/today_risky.sql
+++ models/schedule/daily/5am/normalised/today_risky.sql
@@ -4,13 +4,13 @@ select distinct user_id,'end of gift' as status,box_sub_status
 from sales.box_sales bs
 inner join `teamdata-291012.user.customers` c using(user_id,dw_country_code)
 where bs.gift = 1 AND bs.last_committed_box = 1
-and date='2025-02-01' and dw_country_code='FR'
+and date='2025-02-01' and dw_country_code='FR' 
 union all
 
 -- moins de 7 box et sub
 select distinct user_id ,'moins de 7 box',box_sub_status
 from `teamdata-291012.user.customers`
-where nb_box_paid <7
+where nb_box_paid <7 and dw_country_code='FR'
 and box_sub_status='SUB'
 
 -- les acquiz mega discount
@@ -33,6 +33,6 @@ left join {{ ref('today_new') }} n on n.user_id=t.user_id
 left join {{ ref('today_whales') }} w on w.user_id=t.user_id 
 left join {{ ref('today_stars') }} stars on stars.user_id=t.user_id 
 where p.user_id is null and sp.user_id is null and n.user_id is null and w.user_id is null and stars.user_id is null
-
+and t.user_id=2333779
 group by all
 
