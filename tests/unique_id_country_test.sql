abcf837fd1761e55955518571deb06b4300e1f69
matblissim <mathieu.helie@blissim.fr>
2025-04-03 17:27:16 +0200

    test

--- tests/unique_id_country_test.sql
+++ tests/unique_id_country_test.sql
@@ -1,16 +1,9 @@
--- tests/unique_id_country_multi_folders.sql
-
--- Liste des modèles de différents dossiers
 {% set models_to_test = [
-    -- Modèles du dossier 'merge'
     'users',
     'tags'
-    
-    -- Ajoutez tous les autres modèles que vous souhaitez tester
 ] %}
 
 {% for model_name in models_to_test %}
-
     {% if not loop.first %}
         union all
     {% endif %}
@@ -25,5 +18,4 @@
       and dw_country_code is not null
     group by id, dw_country_code
     having count(*) > 1
-
 {% endfor %}
