7170ac452996cf2d4985034e90dcc074066c4f2f
matblissim <mathieu.helie@blissim.fr>
2025-04-07 19:30:13 +0200

    prospects

--- models/schedule/daily/5am/normalised/today_prospects.sql
+++ models/schedule/daily/5am/normalised/today_prospects.sql
@@ -1,12 +1,13 @@
 select distinct user_id,
-last_box_paid_date,
+last_box_paid_date,last_shop_order_date,
 box_sub_status as status_lvl1,
 'PROSPECTS' as status
  from teamdata-291012.user.customers 
 where (date_diff(current_date(),last_box_paid_date,month) >36 
 or
-last_box_paid_date is null
-
+last_box_paid_date is null) and (
+ date_diff(current_date(),last_shop_order_date,month) >36 
+or last_shop_order_date is null
  )
 and box_sub_status in ('CHURN','NEVERSUB')
 
