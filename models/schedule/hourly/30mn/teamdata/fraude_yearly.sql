09722ee3cc246e168844cdbd69d34adaf277c72a
matblissim <mathieu.helie@blissim.fr>
2025-05-09 13:31:36 +0200

    c

--- models/schedule/hourly/30mn/teamdata/fraude_yearly.sql
+++ models/schedule/hourly/30mn/teamdata/fraude_yearly.sql
@@ -12,7 +12,7 @@ with histo as (
     concat (string_agg(lastname ),' _ ', string_agg(firstname)) as lastname,
     max(order_id) as fraud_order,
     billing_zipcode,
-    string_agg(email)histo_email,
+    string_agg(email) as histo_email,
     count(distinct sub_id) as nb,
     lower(regexp_replace(billing_adress, r'[^a-zA-Z0-9]', '')) as billing_adress
   from {{ ref('box_sales') }} bs
@@ -33,6 +33,7 @@ main as (
     email,
     c.billing_zipcode,
     c.billing_city,
+    histo_email,
     bs.payment_date,
     count(distinct bs.sub_id) as nb,
     histo.nb as histo_fraude,
