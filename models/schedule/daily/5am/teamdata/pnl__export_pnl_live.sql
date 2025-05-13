17ba82050f9ef2cbc23fe595c408bd5649940ef5
matblissim <mathieu.helie@blissim.fr>
2025-05-13 10:07:43 +0200

    pnl

--- models/schedule/daily/5am/teamdata/pnl__export_pnl_live.sql
+++ models/schedule/daily/5am/teamdata/pnl__export_pnl_live.sql
@@ -1,2 +1,17 @@
+{{ 
+  config(
+    materialized = 'incremental',
+    unique_key = ['archive_date', 'period', 'key'],
+    incremental_strategy = 'insert_overwrite',
+    partition_by = {
+      'field': 'archive_date',
+      'data_type': 'date'
+    }
+  ) 
+}}
+
 SELECT current_date AS archive_date, period, key, value 
 FROM `teamdata-291012.pnl.export_pnl_live`
+{% if is_incremental() %}
+WHERE CURRENT_DATE() > (SELECT MAX(archive_date) FROM {{ this }})
+{% endif %}
