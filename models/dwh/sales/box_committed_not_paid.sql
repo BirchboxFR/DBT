8b7ed020474007976c82a00f5ecd747dc1bcfaaf
SoniaBocca <sonia.bocca@blissim.fr>
2025-05-27 20:55:28 +0200

    box committed not paid - retrait de la ref a box_sales

--- models/dwh/sales/box_committed_not_paid.sql
+++ models/dwh/sales/box_committed_not_paid.sql
@@ -3,7 +3,7 @@ FROM {{ ref('orders') }} o
 INNER JOIN {{ ref('order_details') }} d ON o.id = d.order_id AND o.dw_country_code = d.dw_country_code
 INNER JOIN {{ ref('order_detail_sub') }} s ON s.order_detail_id = d.id AND s.dw_country_code = d.dw_country_code
 INNER JOIN {{ ref('boxes') }} b ON b.id = s.box_id AND b.dw_country_code = s.dw_country_code
-LEFT JOIN {{ ref('box_sales') }} bs ON bs.dw_country_code = o.dw_country_code AND bs.user_id = o.user_id AND bs.box_id = s.box_id
+LEFT JOIN sales.box_sales bs ON bs.dw_country_code = o.dw_country_code AND bs.user_id = o.user_id AND bs.box_id = s.box_id
 LEFT JOIN {{ ref('box_paused') }} bp ON bp.dw_country_code = o.dw_country_code AND bp.user_id = o.user_id AND bp.box_id = s.box_id
 WHERE o.status_id IN (1,3) 
 AND s.cannot_suspend = 1
