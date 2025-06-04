9d52ef72183fcb90ddcd76ef7f70800943678236
matblissim <mathieu.helie@blissim.fr>
2025-06-04 16:53:11 +0200

    choose

--- models/schedule/custom/Choose_by_user.sql
+++ models/schedule/custom/Choose_by_user.sql
@@ -1,5 +1,5 @@
    SELECT user_id,
-   concat(cu.dw_country_code,'_',c.user_id)as user_key
+   concat(cu.dw_country_code,'_',c.user_id)as user_key,
     box_id, cc.choice_name, cu.dw_country_code, cu.status_id, cu.created_at as choice_date, cf.name as form_name
     FROM {{ ref('choose_users') }} cu
     JOIN {{ ref('choose_forms') }} cf ON cf.id = cu.form_id AND cf.dw_country_code = cu.dw_country_code
