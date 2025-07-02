33dbbe7fbd5b2e78df9919687296801e3419c195
matblissim <mathieu.helie@blissim.fr>
2025-07-02 14:37:26 +0200

    schéma et all pays

--- models/dwh/inter/merge/products_detailed_rating.sql
+++ models/dwh/inter/merge/products_detailed_rating.sql
@@ -1,3 +1,4 @@
+
 WITH source_data AS (
     -- Données France
     SELECT 
@@ -8,12 +9,15 @@ WITH source_data AS (
         field_slug,
         product_id,
         updated_at,
-        'FR' as country_code,
+        'FR' as dw_country_code,
         `_ab_cdc_updated_at` as source_updated_at
     FROM `teamdata-291012.bdd_prod_fr.wp_jb_products_detailed_rating`
     WHERE `_ab_cdc_deleted_at` IS NULL
     {% if is_incremental() %}
-        AND `_ab_cdc_updated_at` > (SELECT MAX(source_updated_at) FROM {{ this }})
+        AND `_ab_cdc_updated_at` >= (
+            SELECT COALESCE(MAX(source_updated_at), '1900-01-01') 
+            FROM {{ this }}
+        )
     {% endif %}
 
     UNION ALL
@@ -27,12 +31,59 @@ WITH source_data AS (
         field_slug,
         product_id,
         updated_at,
-        'DE' as country_code,
+        'DE' as dw_country_code,
         `_ab_cdc_updated_at` as source_updated_at
     FROM `teamdata-291012.bdd_prod_de.wp_jb_products_detailed_rating`
     WHERE `_ab_cdc_deleted_at` IS NULL
     {% if is_incremental() %}
-        AND `_ab_cdc_updated_at` > (SELECT MAX(source_updated_at) FROM {{ this }})
+        AND `_ab_cdc_updated_at` >= (
+            SELECT COALESCE(MAX(source_updated_at), '1900-01-01') 
+            FROM {{ this }}
+        )
+    {% endif %}
+
+    UNION ALL
+
+    -- Données Espagne
+    SELECT 
+        id,
+        rating,
+        created_at,
+        field_name,
+        field_slug,
+        product_id,
+        updated_at,
+        'ES' as dw_country_code,
+        `_ab_cdc_updated_at` as source_updated_at
+    FROM `teamdata-291012.bdd_prod_es.wp_jb_products_detailed_rating`
+    WHERE `_ab_cdc_deleted_at` IS NULL
+    {% if is_incremental() %}
+        AND `_ab_cdc_updated_at` >= (
+            SELECT COALESCE(MAX(source_updated_at), '1900-01-01') 
+            FROM {{ this }}
+        )
+    {% endif %}
+
+    UNION ALL
+
+    -- Données Italie
+    SELECT 
+        id,
+        rating,
+        created_at,
+        field_name,
+        field_slug,
+        product_id,
+        updated_at,
+        'IT' as dw_country_code,
+        `_ab_cdc_updated_at` as source_updated_at
+    FROM `teamdata-291012.bdd_prod_it.wp_jb_products_detailed_rating`
+    WHERE `_ab_cdc_deleted_at` IS NULL
+    {% if is_incremental() %}
+        AND `_ab_cdc_updated_at` >= (
+            SELECT COALESCE(MAX(source_updated_at), '1900-01-01') 
+            FROM {{ this }}
+        )
     {% endif %}
 )
 
