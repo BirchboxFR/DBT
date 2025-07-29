ad3f0fae8a1ba1ec19ce55e88c661048be083f71
matblissim <mathieu.helie@blissim.fr>
2025-07-29 16:20:10 +0200

    sra test hook en micro code

--- models/dwh/inter/merge/sra_test.sql
+++ models/dwh/inter/merge/sra_test.sql
@@ -1,19 +1,26 @@
+{%- set countries = [
+    {'code': 'FR', 'dataset': 'prod_fr'}
+] -%}
+
 {{ config(
+    materialized='incremental',
+    unique_key=['dw_country_code', 'id'],
     post_hook=[
-        "{{ log('=== DEBUT POST-HOOK ===', info=true) }}",
+        {%- for country in countries %}
         "DELETE FROM `teamdata-291012.prod_fr.sra_test`
-WHERE (id) IN (
+WHERE dw_country_code = '{{ country.code }}' AND (id) IN (
   SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
-  FROM `teamdata-291012.airbyte_internal.prod_fr_raw__stream_wp_jb_survey_result_answers`
+  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_survey_result_answers`
   WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
     AND TIMESTAMP(_airbyte_extracted_at) >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
-)",
-        "{{ log('=== FIN POST-HOOK ===', info=true) }}"
+)"{{ "," if not loop.last }}
+        {%- endfor %}
     ]
 ) }}
 
+{%- for country in countries %}
 SELECT 
-  'FR' as dw_country_code,
+  '{{ country.code }}' as dw_country_code,
   id,
   date,
   ranking,
@@ -22,12 +29,10 @@ SELECT
   created_at,
   updated_at,
   question_id
-FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers`
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_survey_result_answers`
 WHERE `_ab_cdc_deleted_at` IS NULL
-
 {% if is_incremental() %}
-  -- Lookback de 2 heures pour capturer les mises à jour tardives
   AND DATETIME(`_airbyte_extracted_at`) >= DATETIME_SUB(CURRENT_DATETIME(), INTERVAL 2 HOUR)
 {% endif %}
-
-
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
