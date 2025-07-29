308cdb8bd16185f97a4967f9a62de57261924ba2
matblissim <mathieu.helie@blissim.fr>
2025-07-29 12:22:46 +0200

    sra test v4

--- models/dwh/inter/merge/sra_test.sql
+++ models/dwh/inter/merge/sra_test.sql
@@ -1,20 +1,10 @@
--- models/wp_jb_survey_result_answers.sql
--- Modèle dbt avec merge incremental natif et lookback 2h
-
-{{
-  config(
-
-    post_hook="DELETE FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers` 
-               WHERE id IN (
-                 SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64) AS id
-                 FROM `teamdata-291012.airbyte_internal.prod_fr_raw__stream_wp_jb_survey_result_answers`
-                 WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') >= CAST(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR) AS STRING)
-               )"
-  )
-}}
+{% if is_incremental() %}
+  -- Post-hook seulement en mode incrémental
+  {{ config(post_hook="DELETE FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers` WHERE id IN (SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64) FROM `teamdata-291012.airbyte_internal.prod_fr_raw__stream_wp_jb_survey_result_answers` WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL AND TIMESTAMP(JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at')) >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR))") }}
+{% endif %}
 
 SELECT 
-'FR' as dw_country_code,
+  'FR' as dw_country_code,
   id,
   date,
   ranking,
@@ -23,10 +13,12 @@ SELECT
   created_at,
   updated_at,
   question_id
-FROM teamdata-291012.prod_fr.wp_jb_survey_result_answers
+FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers`
 WHERE `_ab_cdc_deleted_at` IS NULL
 
 {% if is_incremental() %}
   -- Lookback de 2 heures pour capturer les mises à jour tardives
-  AND datetime(`_airbyte_extracted_at`) >= DATETIME_SUB(CURRENT_DATETIME(), INTERVAL 2 HOUR)
+  AND DATETIME(`_airbyte_extracted_at`) >= DATETIME_SUB(CURRENT_DATETIME(), INTERVAL 2 HOUR)
 {% endif %}
+
+
