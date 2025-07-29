77cee30e6dce6b5572c5de5c2805deeca4c4bc09
matblissim <mathieu.helie@blissim.fr>
2025-07-29 16:04:50 +0200

    sra test hook v2

--- models/dwh/inter/merge/sra_test.sql
+++ models/dwh/inter/merge/sra_test.sql
@@ -1,15 +1,16 @@
-
 {{ config(
-    post_hook="DELETE FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers`
+    post_hook=[
+        "{{ log('=== DEBUT POST-HOOK ===', info=true) }}",
+        "DELETE FROM `teamdata-291012.prod_fr.wp_jb_survey_result_answers`
 WHERE (id) IN (
-  SELECT
-    CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
+  SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
   FROM `teamdata-291012.airbyte_internal.prod_fr_raw__stream_wp_jb_survey_result_answers`
   WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
     AND TIMESTAMP(_airbyte_extracted_at) >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
-)"
+)",
+        "{{ log('=== FIN POST-HOOK ===', info=true) }}"
+    ]
 ) }}
-
 
 SELECT 
   'FR' as dw_country_code,
