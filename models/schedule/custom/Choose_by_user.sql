9993a7a8e94ce8d64f57598e5e060972488cc790
matblissim <mathieu.helie@blissim.fr>
2025-08-11 08:42:05 +0200

    formd id et choice id dans choose by user

--- models/schedule/custom/Choose_by_user.sql
+++ models/schedule/custom/Choose_by_user.sql
@@ -1,6 +1,7 @@
    SELECT cu.user_id,
    concat(cu.dw_country_code,'_',cu.user_id)as user_key,
-    box_id, cc.choice_name, cu.dw_country_code, cu.status_id, cu.created_at as choice_date, cf.name as form_name
+    box_id, cc.choice_name, cu.dw_country_code, cu.status_id, cu.created_at as choice_date, cf.name as form_name,cf.id as form_id,
+    cc.id as choice_id
     FROM {{ ref('choose_users') }} cu
     JOIN {{ ref('choose_forms') }} cf ON cf.id = cu.form_id AND cf.dw_country_code = cu.dw_country_code
     JOIN {{ ref('choose_choices') }} cc ON cc.id = cu.choice_id AND cc.dw_country_code = cu.dw_country_code
