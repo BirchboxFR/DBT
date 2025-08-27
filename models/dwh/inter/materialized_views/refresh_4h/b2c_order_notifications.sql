1be111d31ee7aa9655ede506c27f1d152aba2092
matblissim <mathieu.helie@blissim.fr>
2025-08-27 15:42:05 +0200

    wp_jb B2C

--- models/dwh/inter/materialized_views/refresh_4h/b2c_order_notifications.sql
+++ models/dwh/inter/materialized_views/refresh_4h/b2c_order_notifications.sql
@@ -14,10 +14,10 @@
 {%- set delete_hooks = [] -%}
 {%- for country in countries -%}
   {%- set delete_sql -%}
-DELETE FROM `teamdata-291012.{{ country.dataset }}.b2c_order_notifications` 
+DELETE FROM `teamdata-291012.{{ country.dataset }}.wp_jb_b2c_order_notifications` 
 WHERE (id) IN (
   SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
-  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_b2c_order_notifications`
+  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_b2c_order_notifications`
   WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
     AND TIMESTAMP(_airbyte_extracted_at) >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
 )
@@ -42,7 +42,7 @@ SELECT
   updated_at,
   question_id,
   _airbyte_extracted_at
-FROM `teamdata-291012.{{ country.dataset }}.b2c_order_notifications`
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_b2c_order_notifications`
 WHERE `_ab_cdc_deleted_at` IS NULL
 {% if is_incremental() %}
   AND DATETIME(`_airbyte_extracted_at`) >= DATETIME_SUB(CURRENT_DATETIME(), INTERVAL 2 HOUR)
