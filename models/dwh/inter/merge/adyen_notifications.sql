df5d777646e05e4c5dca3c7c2c837f8acd3f5f9f
matblissim <mathieu.helie@blissim.fr>
2025-09-15 09:46:02 +0200

    adyen airbyte

--- models/dwh/inter/merge/adyen_notifications.sql
+++ models/dwh/inter/merge/adyen_notifications.sql
@@ -1,85 +1,53 @@
-
-{{
-  config(
-    materialized = 'table',
-    partition_by = {
-      "field": "eventdate",
-      "data_type": "date",
-      "granularity": "month"
+{{ config(
+    partition_by={
+      "field": "_airbyte_extracted_at", 
+      "data_type": "timestamp",
+      "granularity": "day"
     },
-    cluster_by = ['dw_country_code'] 
-  )
-}}
+    cluster_by=["dw_country_code", "id"]
+) }}
+
+{%- set countries = var('survey_countries') -%}
+
+--- partie pays
+
+{%- set delete_hooks = [] -%}
+{%- for country in countries -%}
+  {%- set delete_sql -%}
+DELETE FROM `teamdata-291012.{{ country.dataset }}.wp_jb_adyen_notifications` 
+WHERE (id) IN (
+  SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
+  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_adyen_notifications`
+  WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
+    AND _airbyte_extracted_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
+)
+  {%- endset -%}
+  {%- do delete_hooks.append(delete_sql) -%}
+{%- endfor -%}
+
+{{ config(
+    post_hook=delete_hooks
+) }}
+-- debug
+
+{%- for country in countries %}
+SELECT 
+  '{{ country.code }}' as dw_country_code,
+  id,
+  date,
+  ranking,
+  answer_id,
+  result_id,
+  created_at,
+  updated_at,
+  question_id,
+  _airbyte_extracted_at
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_adyen`
+WHERE `_ab_cdc_deleted_at` IS NULL
+{% if is_incremental() %}
+  AND `_airbyte_extracted_at` >= timestamp_SUB(CURRENT_timestamp(), INTERVAL 2 HOUR)
+{% endif %}
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
 
 
-{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_fr', identifier='wp_jb_adyen_notifications')) -%}
-{%- set de_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_de', identifier='wp_jb_adyen_notifications')) -%}
-{%- set es_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_es', identifier='wp_jb_adyen_notifications')) -%}
-{%- set it_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_it', identifier='wp_jb_adyen_notifications')) -%}
--- Le nombre d'heures en arrière pour lesquelles récupérer les données (4 heures par défaut)
-{%- set lookback_hours = 4 -%}
-
--- Sélection des données françaises
-SELECT 'FR' AS dw_country_code,
-id,merchantaccountcode,value,pspreference,eventcode,success,reason,merchantreference,
-originalreference,order_id,detail_id,sub_id,transaction_time,created_at,updated_at,
-safe_cast(eventdate as date) as eventdate
-FROM `bdd_prod_fr.wp_jb_adyen_notifications` t
-WHERE 
-  -- Filtre sur les lignes non supprimées (CDC)
-  t._ab_cdc_deleted_at IS NULL 
-  -- Filtre sur les données récentes uniquement
-  {% if is_incremental() %}
-  AND t._ab_cdc_updated_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
-  {% endif %}
-UNION ALL
-
-SELECT 'DE' AS dw_country_code,
-id,merchantaccountcode,value,pspreference,eventcode,success,reason,merchantreference,
-originalreference,order_id,detail_id,sub_id,transaction_time,created_at,updated_at,
-safe_cast(eventdate as date) as eventdate
-FROM `bdd_prod_de.wp_jb_adyen_notifications` t
-WHERE 
-  {% if '__deleted' in de_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
-  {% if is_incremental() %}
-  (
-    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
-
-  )
-  {% else %}
-  TRUE
-  {% endif %}
-UNION ALL
-
-SELECT 'ES' AS dw_country_code,
-id,merchantaccountcode,value,pspreference,eventcode,success,reason,merchantreference,
-originalreference,order_id,detail_id,sub_id,transaction_time,created_at,updated_at,
-safe_cast(eventdate as date) as eventdate
-FROM `bdd_prod_es.wp_jb_adyen_notifications` t
-WHERE 
-  {% if '__deleted' in es_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
-  {% if is_incremental() %}
-  (
-    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
-
-  )
-  {% else %}
-  TRUE
-  {% endif %}
-UNION ALL
-
-SELECT 'IT' AS dw_country_code,
-id,merchantaccountcode,value,pspreference,eventcode,success,reason,merchantreference,
-originalreference,order_id,detail_id,sub_id,transaction_time,created_at,updated_at,
-safe_cast(eventdate as date) as eventdate
-FROM `bdd_prod_it.wp_jb_adyen_notifications` t
-WHERE 
-  {% if '__deleted' in it_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) AND{% endif %}
-  {% if is_incremental() %}
-  (
-    t._rivery_last_update >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ lookback_hours }} HOUR)
-
-  )
-  {% else %}
-  TRUE
-  {% endif %}
