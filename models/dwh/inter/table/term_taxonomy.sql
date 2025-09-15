20e2229db88145b6e4fb8514707dc5f749615200
matblissim <mathieu.helie@blissim.fr>
2025-09-15 10:13:49 +0200

    terms en table car pas d'id

---
+++ models/dwh/inter/table/term_taxonomy.sql
@@ -0,0 +1,25 @@
+{{ config(
+    partition_by={
+      "field": "_airbyte_extracted_at", 
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=["dw_country_code"]
+) }}
+
+{%- set countries = var('survey_countries') -%}
+
+
+
+{%- for country in countries %}
+SELECT 
+  '{{ country.code }}' as dw_country_code,
+  b.*
+FROM `teamdata-291012.{{ country.dataset }}.wp_term_taxonomy` b
+WHERE `_ab_cdc_deleted_at` IS NULL
+{% if is_incremental() %}
+  AND `_airbyte_extracted_at` >= timestamp_SUB(CURRENT_timestamp(), INTERVAL 2 HOUR)
+{% endif %}
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
+
