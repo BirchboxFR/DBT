28169311861a4d480b35ffcfd1572f6ac7a6b13d
matblissim <mathieu.helie@blissim.fr>
2025-09-16 17:44:34 +0200

    crm data imagino

--- models/schedule/daily/5am/teamdata/crm_data.sql
+++ models/schedule/daily/5am/teamdata/crm_data.sql
@@ -13,3 +13,24 @@
     FROM {{ ref('splio_data_dedup') }} 
     WHERE event_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 36 MONTH)
     GROUP BY ALL
+    union all
+     
+
+
+     -- imagino part
+ select custom_country,address,null,
+ MAX(lower(t.type)  = 'open') AS open_email,
+         MAX(lower(t.type) = 'click') AS click,
+         date(MAX(CASE WHEN lower(t.type) = 'open' THEN eventdate END)) AS date_last_open_email,
+         date(MAX(CASE WHEN lower(t.type) = 'click' THEN eventdate END)) AS date_last_click_email,
+         SAFE_DIVIDE(COUNTIF(lower(t.type) = 'click' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)), COUNTIF(lower(t.type) = 'done' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))) AS ltm_client_email_rate,
+         SAFE_DIVIDE(COUNTIF(lower(t.type) = 'open' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)), COUNTIF(lower(t.type) = 'done' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))) AS ltm_open_email_rate,
+         COUNTIF(lower(t.type) = 'click' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)) AS ltm_click_email,
+         COUNTIF(lower(t.type) = 'open' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)) AS ltm_open_email,
+         COUNTIF(lower(t.type) = 'done' AND date(eventdate) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)) AS ltm_nb_email
+ FROM cdpimagino.imaginoreplicatedcampaign c
+ LEFT JOIN cdpimagino.imaginoreplicatedtracking t ON t.activationid = c.id
+--where t.address='clementine.clement+compteboximagino@blissim.fr'
+group by all
+ 
+  
