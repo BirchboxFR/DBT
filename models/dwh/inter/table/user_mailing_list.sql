2a5707fde82142bb797b25c712c33d18cab5a3d9
matblissim <mathieu.helie@blissim.fr>
2025-09-22 12:05:14 +0200

    tables user

---
+++ models/dwh/inter/table/user_mailing_list.sql
@@ -0,0 +1,23 @@
+{{ config(
+    partition_by={
+      "field": "_airbyte_extracted_at", 
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=["dw_country_code"]
+) }}
+
+{%- set countries = var('survey_countries') -%}
+
+--- partie pays
+
+
+{%- for country in countries %}
+SELECT 
+  '{{ country.code }}' as dw_country_code,
+  b.*
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_user_mailing_list` b
+
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
+
