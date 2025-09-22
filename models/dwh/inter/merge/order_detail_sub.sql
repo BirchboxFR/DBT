166ccc844020bf549d204e0aca325eba60316df6
matblissim <mathieu.helie@blissim.fr>
2025-09-22 17:52:18 +0200

    order_DEtail_sub

--- models/dwh/inter/merge/order_detail_sub.sql
+++ models/dwh/inter/merge/order_detail_sub.sql
@@ -14,10 +14,10 @@
 {%- set delete_hooks = [] -%}
 {%- for country in countries -%}
   {%- set delete_sql -%}
-DELETE FROM `teamdata-291012.{{ country.dataset }}.wp_jb_brands` 
+DELETE FROM `teamdata-291012.{{ country.dataset }}.wp_jb_order_detail_sub` 
 WHERE (id) IN (
   SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
-  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_brands`
+  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_order_detail_sub`
   WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
     AND _airbyte_extracted_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
 )
@@ -34,7 +34,7 @@ WHERE (id) IN (
 SELECT 
   '{{ country.code }}' as dw_country_code,
   b.*
-FROM `teamdata-291012.{{ country.dataset }}.wp_jb_brands` b
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_order_detail_sub` b
 WHERE `_ab_cdc_deleted_at` IS NULL
 {% if is_incremental() %}
   AND `_airbyte_extracted_at` >= timestamp_SUB(CURRENT_timestamp(), INTERVAL 2 HOUR)
