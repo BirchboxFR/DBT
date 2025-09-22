4a44e137ae1c866f481643ead80960d54b994c2c
matblissim <mathieu.helie@blissim.fr>
2025-09-22 16:13:08 +0200

    tags

--- models/dwh/inter/table/tags.sql
+++ models/dwh/inter/table/tags.sql
@@ -1,37 +1,44 @@
--- Configuration pour table complète (pas incrémental)
 {{ config(
     partition_by={
-      "field": "link_id",
-      "data_type": "int64",
-      "range": {
-        "start": 0,
-        "end": 100000000,
-        "interval": 100000
-      }
+      "field": "_airbyte_extracted_at", 
+      "data_type": "timestamp",
+      "granularity": "day"
     },
-    cluster_by=['dw_country_code']
+    cluster_by=["dw_country_code", "id"]
 ) }}
 
-{% set countries = [
-  {'code': 'FR', 'schema': 'bdd_prod_fr'},
-  {'code': 'DE', 'schema': 'bdd_prod_de'},
-  {'code': 'ES', 'schema': 'bdd_prod_es'},
-  {'code': 'IT', 'schema': 'bdd_prod_it'}
-] %}
+{%- set countries = var('survey_countries') -%}
 
--- Union de tous les pays
-{% for country in countries %}
+--- partie pays
+
+{%- set delete_hooks = [] -%}
+{%- for country in countries -%}
+  {%- set delete_sql -%}
+DELETE FROM `teamdata-291012.{{ country.dataset }}.wp_jb_tags` 
+WHERE (id) IN (
+  SELECT CAST(JSON_EXTRACT_SCALAR(_airbyte_data, '$.id') AS INT64)
+  FROM `teamdata-291012.airbyte_internal.{{ country.dataset }}_raw__stream_wp_jb_tags`
+  WHERE JSON_EXTRACT_SCALAR(_airbyte_data, '$._ab_cdc_deleted_at') IS NOT NULL
+    AND _airbyte_extracted_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR)
+)
+  {%- endset -%}
+  {%- do delete_hooks.append(delete_sql) -%}
+{%- endfor -%}
+
+{{ config(
+    post_hook=delete_hooks
+) }}
+-- debug
+
+{%- for country in countries %}
 SELECT 
-  '{{ country.code }}' AS dw_country_code,
-  t.id,
-  t.link_id,
-  t.value,
-  t.type,
-  t.timestamp,
-  t.user_id,
-  t._airbyte_extracted_at
-FROM `{{ country.schema }}.wp_jb_tags` t
-{% if not loop.last %}
-UNION ALL
+  '{{ country.code }}' as dw_country_code,
+  b.*
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_tags` b
+WHERE `_ab_cdc_deleted_at` IS NULL
+{% if is_incremental() %}
+  AND `_airbyte_extracted_at` >= timestamp_SUB(CURRENT_timestamp(), INTERVAL 2 HOUR)
 {% endif %}
-{% endfor %}
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
+
