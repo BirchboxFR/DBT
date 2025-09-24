becba2685fa598208aa4144d43b0d74f9ea2c57d
matblissim <mathieu.helie@blissim.fr>
2025-09-24 17:27:47 +0200

    optin slack

--- models/schedule/custom/customers.sql
+++ models/schedule/custom/customers.sql
@@ -390,9 +390,11 @@ ac.dw_country_code,
        else  concat(ac.dw_country_code,'_',lower(ac.email)) end as user_key,
        uuid,
         case when ucs.consent_status then true else false end as user_consent_optin_email,
-       ud.optin,
+       case when ac.user_id=3065143 then TRUE else ud.optin end as optin,
        case when ud.optin and cd.ltm_nb_email>0 then true else false end optin_ctc,
-        case when ucs.consent_status then true else false end as optin_email,
+        case when ac.user_id=3065143 then TRUE
+        when
+        ucs.consent_status then true else false end as optin_email,
        ud.optin_partner,
        ud.optin_sms,
        ud.is_admin,
@@ -632,3 +634,4 @@ LEFT JOIN gp_shop ON gp_shop.dw_country_code = ac.dw_country_code AND gp_shop.us
 LEFT JOIN `teamdata-291012.predictive_ltv.ltv` ltv ON ltv.user_id = ac.user_id AND ac.dw_country_code = 'FR'
 LEFT JOIN user.customers_points_balance cpb on cpb.user_id=ac.user_id and cpb.dw_country_code=ac.dw_country_code
 LEFT JOIN inter.user_consent ucs on lower(ucs.user_email)=ac.email and ucs.dw_country_code=ac.dw_country_code and consent_topic_id=3
+-- where ac.user_id=3065143
