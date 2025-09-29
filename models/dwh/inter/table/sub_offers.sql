1af8c39e1d414ab105568a9d6a9a23312cdf33e8
matblissim <mathieu.helie@blissim.fr>
2025-09-29 16:55:40 +0200

    sub_offers 3.0

--- models/dwh/inter/table/sub_offers.sql
+++ models/dwh/inter/table/sub_offers.sql
@@ -1,38 +1,54 @@
 {{ config(
-    cluster_by=["dw_country_code"]
+  materialized='incremental',
+  unique_key=['dw_country_code','id'],
+  partition_by={
+    "field": "_airbyte_extracted_at",
+    "data_type": "timestamp",
+    "granularity": "day"
+  },
+  cluster_by=["dw_country_code","id"]
 ) }}
 
 {%- set countries = var('survey_countries') -%}
+{%- set window_hours = 4 -%}
+{%- set window_start -%}
+TIMESTAMP_TRUNC(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ window_hours }} HOUR), DAY)
+{%- endset -%}
 
---- partie pays
-
-{%- for country in countries %}
-SELECT 
-  '{{ country.code }}' as dw_country_code,
-     id,
-    code,
-    count,
-    title,
-    max_use,
-    trigger,
-    start_box,
-    conditions,
-    created_at,
-    created_by,
-    offer_type,
-    updated_at,
-    valid_from,
-    description,
-    offer_value,
-    offer_target,
-    user_max_use,
-    validity_date,
-    parent_offer_id,
-    subs_paid_in_advance,
-    secondary_offer_value,
-    sub_engagement_period
-FROM `teamdata-291012.{{ country.dataset }}.wp_jb_sub_offers` b
-
-{{ "UNION ALL" if not loop.last }}
+{%- set delete_hooks = [] -%}
+{%- for country in countries -%}
+  {%- set delete_sql -%}
+DELETE FROM inter.sub_offers
+WHERE dw_country_code = '{{ country.code }}'
+  -- prune partitions de TA table
+  AND _airbyte_extracted_at >= {{ window_start }}
+  AND id IN (
+    SELECT CAST(d.id AS INT64)
+    FROM `teamdata-291012.{{ country.dataset }}.wp_jb_sub_offers` AS d
+    -- prune partitions des tables Airbyte
+    WHERE d._airbyte_extracted_at >= {{ window_start }}
+      -- soft delete détecté (STRING → TIMESTAMP)
+      AND SAFE.PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', NULLIF(d._ab_cdc_deleted_at, '')) IS NOT NULL
+  );
+  {%- endset -%}
+  {%- do delete_hooks.append(delete_sql) -%}
 {%- endfor %}
 
+{{ config(post_hook=delete_hooks) }}
+
+{%- for country in countries %}
+SELECT
+  '{{ country.code }}' AS dw_country_code,
+  b.*
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_sub_offers` AS b
+WHERE
+  -- actif = pas de soft delete (gère aussi la chaîne vide)
+  NULLIF(b._ab_cdc_deleted_at, '') IS NULL
+  -- prune partitions source
+  AND b._airbyte_extracted_at >= {{ window_start }}
+{% if is_incremental() %}
+  -- fenêtre glissante pour l'incrémental
+  AND b._airbyte_extracted_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ window_hours }} HOUR)
+{% endif %}
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
