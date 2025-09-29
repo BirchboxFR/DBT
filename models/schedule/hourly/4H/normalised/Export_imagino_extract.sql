9dade093c6c3185d1e13cfac674cedd9448826e2
matblissim <mathieu.helie@blissim.fr>
2025-09-29 10:34:45 +0200

    unsub

--- models/schedule/hourly/4H/normalised/Export_imagino_extract.sql
+++ models/schedule/hourly/4H/normalised/Export_imagino_extract.sql
@@ -17,7 +17,7 @@ campaign_tracking_stats AS (
    count(distinct case when t.type='open' then t.address end) as open_uniques,
    count(distinct case when t.type='click' then t.address end) as click_uniques,
    count( case when t.type='click' then t.address end) as clicks,
-   
+   count(case when url like '%unsubscribe%' then 1 end ) as unsubscribes
  FROM cdpimagino.imaginoreplicatedcampaign c
  LEFT JOIN cdpimagino.imaginoreplicatedtracking t ON t.activationid = c.id
  WHERE DATE(t.eventDate) >= '2024-01-01'  -- Filtre partition
@@ -31,6 +31,7 @@ SELECT
     COALESCE(cts.open_uniques, 0) as open_uniques,
     COALESCE(cts.click_uniques, 0) as click_uniques,
     COALESCE(cts.clicks, 0) as clicks,
+    COALESCE(cts.unsubscribes, 0) as unsubscribes
     
 FROM campaign_message_stats cms
 LEFT JOIN campaign_tracking_stats cts ON cts.campaign_id = cms.id
