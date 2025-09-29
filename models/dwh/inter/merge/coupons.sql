8bb20a7888a1b5fe23342c8de206b5540531a5b7
matblissim <mathieu.helie@blissim.fr>
2025-09-29 16:53:42 +0200

    coupons 3.0

--- models/dwh/inter/merge/coupons.sql
+++ models/dwh/inter/merge/coupons.sql
@@ -1,56 +1,54 @@
 {{ config(
-    cluster_by=["dw_country_code"]
+  materialized='incremental',
+  unique_key=['dw_country_code','id'],
+  partition_by={
+    "field": "_airbyte_extracted_at",
+    "data_type": "timestamp",
+    "granularity": "day"
+  },
+  cluster_by=["dw_country_code","id"]
 ) }}
 
 {%- set countries = var('survey_countries') -%}
+{%- set window_hours = 4 -%}
+{%- set window_start -%}
+TIMESTAMP_TRUNC(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ window_hours }} HOUR), DAY)
+{%- endset -%}
 
---- partie pays
-
-{%- for country in countries %}
-SELECT 
-  '{{ country.code }}' as dw_country_code,
- 
-    id,
-    code,
-    count,
-    max_use,
-    shipping,
-    parent_id,
-    conditions,
-    created_at,
-    created_by,
-    store_only,
-    updated_at,
-    valid_from,
-    description,
-    allow_choose,
-    start_box_id,
-    discount_type,
-    influencer_id,
-    is_for_raffed,
-    validity_date,
-    applies_to_gift,
-    conditions_text,
-    discount_amount,
-    discount_on_sub,
-    excluded_brands,
-    force_start_box,
-    actif_point_rouge,
-    discount_amount_2,
-    excluded_products,
-    influencer_points,
-    keep_sub_discount,
-    special_box_label,
-    can_not_use_points,
-    non_combinable_raf,
-    can_not_gain_points,
-    box_price_adjustment,
-    subs_paid_in_advance,
-    sub_engagement_period,
-    actif_non_discountable,
-    can_use_with_points_voucher
-FROM `teamdata-291012.{{ country.dataset }}.wp_jb_coupons` b
-
-{{ "UNION ALL" if not loop.last }}
+{%- set delete_hooks = [] -%}
+{%- for country in countries -%}
+  {%- set delete_sql -%}
+DELETE FROM inter.coupons
+WHERE dw_country_code = '{{ country.code }}'
+  -- prune partitions de TA table
+  AND _airbyte_extracted_at >= {{ window_start }}
+  AND id IN (
+    SELECT CAST(d.id AS INT64)
+    FROM `teamdata-291012.{{ country.dataset }}.wp_jb_coupons` AS d
+    -- prune partitions des tables Airbyte
+    WHERE d._airbyte_extracted_at >= {{ window_start }}
+      -- soft delete détecté (STRING → TIMESTAMP)
+      AND SAFE.PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%E*S%Ez', NULLIF(d._ab_cdc_deleted_at, '')) IS NOT NULL
+  );
+  {%- endset -%}
+  {%- do delete_hooks.append(delete_sql) -%}
 {%- endfor %}
 
+{{ config(post_hook=delete_hooks) }}
+
+{%- for country in countries %}
+SELECT
+  '{{ country.code }}' AS dw_country_code,
+  b.*
+FROM `teamdata-291012.{{ country.dataset }}.wp_jb_coupons` AS b
+WHERE
+  -- actif = pas de soft delete (gère aussi la chaîne vide)
+  NULLIF(b._ab_cdc_deleted_at, '') IS NULL
+  -- prune partitions source
+  AND b._airbyte_extracted_at >= {{ window_start }}
+{% if is_incremental() %}
+  -- fenêtre glissante pour l'incrémental
+  AND b._airbyte_extracted_at >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {{ window_hours }} HOUR)
+{% endif %}
+{{ "UNION ALL" if not loop.last }}
+{%- endfor %}
