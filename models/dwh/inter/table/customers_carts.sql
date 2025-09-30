e80e10fca520e88766c4131ad6996d7298a1140d
matblissim <mathieu.helie@blissim.fr>
2025-09-30 11:39:31 +0200

    datetime

--- models/dwh/inter/table/customers_carts.sql
+++ models/dwh/inter/table/customers_carts.sql
@@ -1,10 +1,12 @@
-SELECT c.dw_country_code,cart_id,cd.created_at created_utc ,datetime(cd.created_at,'Europe/Paris') as date,concat(c.dw_country_code,'_',user_id) as user_key ,
+SELECT c.dw_country_code,cart_id,c.created_at created_utc ,datetime(cd.created_at,'Europe/Paris') as date,concat(c.dw_country_code,'_',user_id) as user_key ,
 cd.product_id, c.status_id,p.brand_full_name,p.ht_sale_price,p.product_nice_name,p.thumb_url,pro.price,pro.attr_special_sub_price,pro.attr_special_price,
+current_datetime('Europe/Paris') as current_datetime,
+datetime(c.created_at) created_cart_datetime,
 case 
 
-when datetime_diff(current_datetime('Europe/Paris'), datetime(cd.created_at,'Europe/Paris'), hour)>1  and c.status_id in (1,2) then true else false end as  is_abandonned,
-datetime_diff(current_datetime('Europe/Paris'), datetime(cd.created_at,'Europe/Paris'), hour) as cart_age_hours,
-datetime_diff(current_datetime('Europe/Paris'), datetime(cd.created_at,'Europe/Paris'), minute) as cart_age_minutes,
+when datetime_diff(current_datetime('Europe/Paris'), datetime(c.created_at), hour)>1  and c.status_id in (1,2) then true else false end as  is_abandonned,
+datetime_diff(current_datetime('Europe/Paris'), datetime(c.created_at), hour) as cart_age_hours,
+datetime_diff(current_datetime('Europe/Paris'), datetime(c.created_at), minute) as cart_age_minutes,
 sum(cd.qty) as nb_items,
 case when datetime_diff(current_datetime(), c.created_at, hour)<48 then true else false end as winback_possible
 FROM `teamdata-291012.inter.saved_cart` c
@@ -15,6 +17,6 @@ where 1=1
 
 group by all
 --having cart_age_hours < 1000
---having  user_key='FR_2986148'
+having  user_key='FR_626757'
 order by date desc
 --LIMIT 1000
