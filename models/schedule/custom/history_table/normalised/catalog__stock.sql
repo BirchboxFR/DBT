a86585ab6a4ea60fb71b5380f148656ed2aaf68a
matblissim <mathieu.helie@blissim.fr>
2025-10-01 09:21:26 +0200

    history catalog stock

--- models/schedule/custom/history_table/normalised/catalog__stock.sql
+++ models/schedule/custom/history_table/normalised/catalog__stock.sql
@@ -12,5 +12,5 @@
 
 SELECT current_date AS archive_date,  
 s.sku, s.pc_cat1, s.pc_cat2, s.codification, s.brand_full_name, s.name, s.stock, cast(s.sale_price as float64) as sale_price, s.euro_purchase_price, s.bank_reservations, s.available_stock, s.valo_stock, s.valo_available_stock,
-SAFE_CAST(s.dluo_min AS DATE), s.stock_POT1, s.stock_POT2, s.product_class, s.stock_coverage, SAFE_CAST(s.dluo_min AS DATE) AS dluo_min, NULL AS row_num
+SAFE_CAST(s.dluo_min AS DATE) f0_, s.stock_POT1, s.stock_POT2, s.product_class, s.stock_coverage, SAFE_CAST(s.dluo_min AS DATE) AS dluo_min, NULL AS row_num
  FROM `normalised-417010.catalog.stock` s
