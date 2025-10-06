842e7d34a9671da4e782d65d29934efb083e30e2
matblissim <mathieu.helie@blissim.fr>
2025-10-06 15:47:39 +0200

    export campaign

--- models/schedule/hourly/4H/normalised/Export_predictive_ltv_per_campaign.sql
+++ models/schedule/hourly/4H/normalised/Export_predictive_ltv_per_campaign.sql
@@ -15,5 +15,5 @@ FROM `teamdata-291012.funnel.funnel_data_fbclid_event_IDtransaction_ID` t
 inner join (select distinct user_id,dw_country_code,order_id from sales .box_sales where dw_country_code='FR' ) bs on cast(bs.order_id as string)=t.Transaction_ID___GA4__Google_Analytics
 inner join user.customers c on c.user_id=bs.user_id and c.dw_country_code=bs.dw_country_code
 inner join teamdata-291012.predictive_ltv.ltv l on l.user_id=bs.user_id and l.dw_country_code=bs.dw_country_code
-where Session_campaign___GA4__Google_Analytics<>'(not set)' and Session_campaign___GA4__Google_Analytics like 'ACQ%'
+where Session_campaign___GA4__Google_Analytics<>'(not set)' -- and Session_campaign___GA4__Google_Analytics like 'ACQ%'
 group by all
