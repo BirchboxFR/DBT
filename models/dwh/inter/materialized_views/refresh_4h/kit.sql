f0162e188e417c3369b9c1bac32b5c0948df75e8
matblissim <mathieu.helie@blissim.fr>
2025-10-24 18:03:08 +0200

    kit_dedup

--- models/dwh/inter/materialized_views/refresh_4h/kit.sql
+++
@@ -1,6 +0,0 @@
-{%- set fr_columns = adapter.get_columns_in_relation(api.Relation.create(schema='bdd_prod_sublissim', identifier='kit_raw')) -%}
-
-SELECT 
-t.*
-FROM `bdd_prod_sublissim.kit_raw` t
-WHERE {% if '__deleted' in fr_columns | map(attribute='name') %}(t.__deleted is null OR t.__deleted = false) {% else %}true{% endif %}
---
+++ models/dwh/inter/materialized_views/refresh_4h/kit_dedup.sql
@@ -0,0 +1,4 @@
+SELECT 
+t.*
+FROM `bdd_prod_sublissim.kit` t
+where _ab_cdc_deleted_at is null
