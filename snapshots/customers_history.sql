26ae7ade7055115bb268c7861b12f79702ce556d
matblissim <mathieu.helie@blissim.fr>
2025-11-18 10:27:13 +0100

    dzdz

--- snapshots/customers_history.sql
+++ snapshots/customers_history.sql
@@ -6,22 +6,17 @@
       unique_key='user_key',
       strategy='check',
       check_cols=[
-        -- Identité
         'email',
         'firstname',
         'lastname',
         'gender',
         'birth_date',
-        
-        -- Coordonnées
         'billing_phone',
         'billing_region',
         'billing_country',
         'billing_zipcode',
         'billing_city',
         'billing_adress',
-        
-        -- Consentements (changements = important)
         'user_consent_optin_email',
         'optin',
         'optin_ctc',
@@ -29,23 +24,17 @@
         'optin_partner',
         'optin_sms',
         'optin_whatsapp',
-        
-        -- Profil beauté (change rarement, important pour la segmentation)
         'skin_type',
         'skin_complexion',
         'hair_type',
         'hair_color',
         'beauty_budget',
         'skin_tone',
-        
-        -- Statut abonnement (critique pour le business)
         'box_sub_status',
         'current_sub_type',
         'current_is_committed',
         'current_coupon_code',
         'next_box_churn',
-        
-        -- Flags importants
         'is_admin',
         'is_shopper',
         'is_shopper_fullsize',
