93bb1425b37a7bf89337e6c1d5c91d91960534ac
matblissim <mathieu.helie@blissim.fr>
2025-11-21 17:48:06 +0100

    last event date

--- models/schedule/daily/5am/teamdata/device_by_user_id.sql
+++ models/schedule/daily/5am/teamdata/device_by_user_id.sql
@@ -1,3 +1,4 @@
-SELECT distinct user_id as user_key,Device_ID FROM `teamdata-291012.cdpimagino.BQ_imagino_Tag_Event_v2` 
+SELECT distinct user_id as user_key,Device_ID,max(event_date) as last_event_date FROM `teamdata-291012.cdpimagino.BQ_imagino_Tag_Event_v2` 
 where user_id is not null and length(user_id)>3
 and not starts_with(user_id,'_')
+group by all
