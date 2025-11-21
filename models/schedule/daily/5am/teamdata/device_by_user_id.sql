6d66051c5db5ad153a18acec85a572d91575c36b
matblissim <mathieu.helie@blissim.fr>
2025-11-21 11:19:09 +0100

    device id

--- models/schedule/daily/5am/teamdata/device_by_user_id.sql
+++ models/schedule/daily/5am/teamdata/device_by_user_id.sql
@@ -1,2 +1,3 @@
-SELECT distinct user_id,Device_ID FROM `teamdata-291012.cdpimagino.BQ_imagino_Tag_Event_v2` 
+SELECT distinct user_id as user_key,Device_ID FROM `teamdata-291012.cdpimagino.BQ_imagino_Tag_Event_v2` 
 where user_id is not null and length(user_id)>3
+and not starts_with(user_id,'_')
