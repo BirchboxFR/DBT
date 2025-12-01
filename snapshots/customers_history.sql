0a3d3fb13cd0ccfada5d4e7bc4c7ea5a1d58d4e3
matblissim <mathieu.helie@blissim.fr>
2025-12-01 10:02:04 +0100

    snapshot daily 2 eme champ

--- snapshots/customers_history.sql
+++ snapshots/customers_history.sql
@@ -6,50 +6,25 @@
       unique_key='user_key',
       strategy='check',
       check_cols=[
-        'email',
-        'firstname',
-        'lastname',
-        'gender',
-        'birth_date',
-        'billing_phone',
-        'billing_region',
-        'billing_country',
-        'billing_zipcode',
-        'billing_city',
-        'billing_adress',
-        'user_consent_optin_email',
-        'optin',
-        'optin_ctc',
-        'optin_email',
-        'optin_partner',
-        'optin_sms',
-        'optin_whatsapp',
-        'skin_type',
-        'skin_complexion',
-        'hair_type',
-        'hair_color',
-        'beauty_budget',
-        'skin_tone',
-        'box_sub_status',
-        'current_sub_type',
-        'current_is_committed',
-        'current_coupon_code',
-        'next_box_churn',
-        'is_admin',
-        'is_shopper',
-        'is_shopper_fullsize',
-        'is_ever_gifted',
-        'is_raffer'
+        'email','firstname','lastname','gender','birth_date','billing_phone',
+        'billing_region','billing_country','billing_zipcode','billing_city',
+        'billing_adress','user_consent_optin_email','optin','optin_ctc',
+        'optin_email','optin_partner','optin_sms','optin_whatsapp','skin_type',
+        'skin_complexion','hair_type','hair_color','beauty_budget','skin_tone',
+        'box_sub_status','current_sub_type','current_is_committed',
+        'current_coupon_code','next_box_churn','is_admin','is_shopper',
+        'is_shopper_fullsize','is_ever_gifted','is_raffer'
       ]
     )
 }}
 
 select
-  -- take all columns except the ones we will override
-  * except(box_net_revenue, array_boxes),
+  -- exclude all float columns you want to override + other exceptions
+  * except(box_net_revenue, box_average_discount, array_boxes),
 
-  -- re-add box_net_revenue casted to NUMERIC (use CAST(...) if you want failure on bad values)
-  safe_cast(box_net_revenue as numeric) as box_net_revenue
+  -- re-add them casted to NUMERIC (strict)
+  cast(box_net_revenue as numeric) as box_net_revenue,
+  cast(box_average_discount as numeric) as box_average_discount
 
 from {{ ref('customers') }}
 
