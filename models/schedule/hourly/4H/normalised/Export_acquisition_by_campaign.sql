1618e4da9d12d438f0c18b8fef02a90e5a1efef9
matblissim <mathieu.helie@blissim.fr>
2025-12-02 11:19:22 +0100

    campaign date

--- models/schedule/hourly/4H/normalised/Export_acquisition_by_campaign.sql
+++ models/schedule/hourly/4H/normalised/Export_acquisition_by_campaign.sql
@@ -2,6 +2,7 @@
 
 SELECT
   campaign_name,
+  campaign_date,
   SUM(CASE WHEN acquis = TRUE THEN 1 ELSE 0 END) AS total_acquis
 FROM `normalised-417010.crm.crm_acquisitions`
-GROUP BY campaign_name
+GROUP BY all
