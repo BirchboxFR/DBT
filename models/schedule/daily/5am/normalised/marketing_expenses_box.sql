7ae27287d94f47217ad6678b58a42696fc1076c3
SoniaBocca <sonia.bocca@blissim.fr>
2026-01-28 16:40:17 +0100

    add acquis status 2 on marketing expenses box

--- models/schedule/daily/5am/normalised/marketing_expenses_box.sql
+++ models/schedule/daily/5am/normalised/marketing_expenses_box.sql
@@ -7,6 +7,7 @@ cac_data ='1' as is_cac_eligible,
 case when market='France' then 'FR' when market='Spain' then 'ES' when market ='Germany' then 'DE' WHEN market = 'Poland' THEN 'PL' WHEN market = 'Sweden' THEN 'SE' end as dw_country_code,cac_data,
 EXTRACT(YEAR FROM b.date) year,
 EXTRACT(month FROM b.date) month,
+CASE WHEN Acquis_type_  = 'Reac' THEN 'REACTIVATION' WHEN Acquis_type_ = 'New new' THEN 'NEW NEW' ELSE NULL END AS acquis_status_lvl2,
 market,sum(cost) as spent ,sum(impressions) as impressions
 FROM funnel.funnel_data d
 inner join inter.boxes b on d.date>=b.shipping_date  and case when market='France' then 'FR' when market='Spain' then 'ES' when market ='Germany' then 'DE' WHEN market = 'Poland' THEN 'PL' WHEN market = 'Sweden' THEN 'SE' end=b.dw_country_code  AND d.date <= b.closing_Date
