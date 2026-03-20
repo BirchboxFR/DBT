e5a4ea8e39ec75c7d36944ae39648e925d6e79d1
SoniaBocca <sonia.bocca@blissim.fr>
2026-03-20 11:25:18 +0100

    add acquis box in export_GWS_Live

--- models/schedule/hourly/4H/normalised/Export_GWS_LIVE.sql
+++ models/schedule/hourly/4H/normalised/Export_GWS_LIVE.sql
@@ -10,6 +10,18 @@
   )
 }}
 
+
+WITH monthly_avg_cost AS (
+SELECT r.authorized_country, campaign_date, variation_type, AVG(ii.euro_purchase_price) AS monthly_cost
+FROM `teamdata-291012.Spreadsheet_synchro.raw_doc_compo2` r
+JOIN `teamdata-291012.catalog.inventory_item_catalog` ii ON ii.sku = r.sku_compo
+WHERE campaign_date >= '2024-01-01'
+AND variation_type = 'MONTHLY'
+AND r.authorized_country IS NOT NULL
+GROUP BY ALL
+ORDER BY campaign_date DESC
+) 
+
 SELECT 
     bs.dw_country_code,
     bs.date as month, 
@@ -34,4 +46,27 @@ JOIN inter.boxes b
     AND b.date = bs.date 
 WHERE coupon is not null and bs.year>2021
 GROUP BY ALL 
-HAVING cost > 0 
+
+
+UNION ALL
+
+
+SELECT bs.dw_country_code, 
+r.campaign_date,
+date(payment_date) as date, 
+ROUND(SUM(CASE WHEN ii.euro_purchase_price-monthly_avg_cost.monthly_cost > 0 THEN ii.euro_purchase_price-monthly_avg_cost.monthly_cost ELSE 0 END),0) AS cost, 
+CAST(NULL AS STRING) AS coupon,
+'Acquis box' AS type,
+'BOX' AS product_type,
+'ACQUIZ' AS acquis_type,
+shipping_Date as first_day,
+CASE WHEN bs.crm_acquisition THEN 'CRM' ELSE 'OTHER' END AS attribution
+FROM `teamdata-291012.Spreadsheet_synchro.raw_doc_compo2` r
+JOIN monthly_avg_cost USING (campaign_date, authorized_country)
+JOIN `teamdata-291012.catalog.inventory_item_catalog` ii ON ii.sku = r.sku_compo
+JOIN sales.box_sales bs ON bs.dw_country_code = r.authorized_country AND bs.date = r.campaign_date AND bs.coffret_id = r.coffret_id
+JOIN inter.boxes b ON b.dw_country_code = bs.dw_country_code AND b.date = bs.date 
+
+WHERE r.variation_type = 'ACQUIS'
+GROUP BY ALL
+HAVING cost > 0
