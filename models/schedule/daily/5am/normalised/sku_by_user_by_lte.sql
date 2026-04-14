aa678929c1467e06b8fbdb77cfb8cc866d797f1a
matblissim <mathieu.helie@blissim.fr>
2026-04-14 17:35:45 +0200

    user_key

--- models/schedule/daily/5am/normalised/sku_by_user_by_lte.sql
+++ models/schedule/daily/5am/normalised/sku_by_user_by_lte.sql
@@ -14,7 +14,7 @@
   )
 }}
 
-select distinct ss.product_codification,ss.dw_country_code,ss.product_id, ss.product_name lte_name,lower(c.product_nice_name) product_name,  c.brand_full_name as brand_name ,user_id,c.sku,c.category_lvl_1,c.category_lvl_2,c.category_lvl_3 from sales.shop_sales ss
+select distinct ss.user_key,ss.product_codification,ss.dw_country_code,ss.product_id, ss.product_name lte_name,lower(c.product_nice_name) product_name,  c.brand_full_name as brand_name ,user_id,c.sku,c.category_lvl_1,c.category_lvl_2,c.category_lvl_3 from sales.shop_sales ss
 inner join `teamdata-291012.inter.lte_kits` l on l.lte_product_id=ss.product_id and l.dw_country_code=ss.dw_country_code
 inner join product.catalog c on c.product_id=l.product_id and c.dw_country_code=l.dw_country_code
 --inner join product.catalog enfant on enfant.product_id=c.product_id and enfant.dw_country_code=ss.dw_country_code
