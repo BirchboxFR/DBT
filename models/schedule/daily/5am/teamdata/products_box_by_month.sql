a8298857caaf12646831400675d3f236592d739c
matblissim <mathieu.helie@blissim.fr>
2026-04-15 16:51:19 +0200

    products_lte_by_month

--- models/schedule/daily/5am/teamdata/products_box_by_month.sql
+++ models/schedule/daily/5am/teamdata/products_box_by_month.sql
@@ -1,3 +1,3 @@
-select distinct box_date,dw_country_code,component_sku,box_id, from `normalised-417010.box.sku_by_user_by_box`
+select distinct box_date,dw_country_code,component_sku,box_id,component_brand_name from `normalised-417010.box.sku_by_user_by_box`
 where box_id>150
 and component_codification_lvl2='Product'
