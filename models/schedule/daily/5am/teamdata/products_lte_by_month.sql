e0726643be6ebd8f968b96a0f02968566fccd840
matblissim <mathieu.helie@blissim.fr>
2026-04-17 15:39:26 +0200

    lte_sku

--- models/schedule/daily/5am/teamdata/products_lte_by_month.sql
+++ models/schedule/daily/5am/teamdata/products_lte_by_month.sql
@@ -1,4 +1,4 @@
-select distinct product_codification,dw_country_code,lte_name,product_name,sku,
+select distinct product_codification,dw_country_code,lte_name,product_name,sku,lte_sku,
 product_categories_lvl2,
  min(DATE(CAST(year AS INT64), CAST(month AS INT64), 1) )AS date_debut from `normalised-417010.shop.sku_by_user_by_lte`
 group by all
