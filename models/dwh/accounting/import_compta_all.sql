d14e5ad061fd71f24524ce45a0b47151b48363be
matblissim <mathieu.helie@blissim.fr>
2026-05-05 09:40:11 +0200

    source

--- models/dwh/accounting/import_compta_all.sql
+++ models/dwh/accounting/import_compta_all.sql
@@ -1,5 +1,5 @@
-SELECT *,'BOX' FROM {{ ref('import_compta_box') }}
+SELECT *,'BOX' as source FROM {{ ref('import_compta_box') }}
 UNION ALL
 SELECT *,'SHOP' FROM {{ ref('import_compta_shop') }}
 UNION ALL
-SELECT *'GIFT' FROM {{ ref('import_compta_gift') }}
+SELECT *,'GIFT' FROM {{ ref('import_compta_gift') }}
